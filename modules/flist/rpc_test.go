e2adb122c99309323ba84458599030fb2f2bb5a8
Christophe de Carvalho <christophe.dcpm@gmail.com>
2019-05-16 08:28:27 +0200

    split RPC test boilerplate into its own function

--- modules/flist/rpc_test.go
+++ modules/flist/rpc_test.go
@@ -11,16 +11,16 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/threefoldtech/zbus"
+	"github.com/threefoldtech/zosv2/modules"
 	"github.com/threefoldtech/zosv2/modules/stubs"
 )
 
-func TestRPCMount(t *testing.T) {
-	require := require.New(t)
-	assert := assert.New(t)
-	f, cleanup := testFlistModule(t)
-	defer cleanup()
+func testPrepareRPC(t *testing.T) (modules.Flister, func()) {
+	const redisAddr = "tcp://localhost:6379"
 
-	server, err := zbus.NewRedisServer("flist", "tcp://localhost:6379", 1)
+	f, cleanup := testFlistModule(t)
+
+	server, err := zbus.NewRedisServer("flist", redisAddr, 1)
 	if err != nil {
 		log.Fatal().Msgf("fail to connect to message broker server: %v\n", err)
 	}
@@ -28,13 +28,22 @@ func TestRPCMount(t *testing.T) {
 	server.Register(zbus.ObjectID{Name: "flist", Version: "0.0.1"}, f)
 	go func() {
 		err := server.Run(context.Background())
-		require.NoError(err)
+		require.NoError(t, err)
 	}()
 
-	client, err := zbus.NewRedisClient("tcp://localhost:6379")
-	require.NoError(err)
+	client, err := zbus.NewRedisClient(redisAddr)
+	require.NoError(t, err)
 
 	flist := stubs.NewFlisterStub(client)
+	return flist, cleanup
+}
+
+func TestRPCMount(t *testing.T) {
+	require := require.New(t)
+	assert := assert.New(t)
+
+	flist, cleanup := testPrepareRPC(t)
+	defer cleanup()
 
 	path, err := flist.Mount("https://hub.grid.tf/zaibon/ubuntu_bionic.flist", "")
 	require.NoError(err)
