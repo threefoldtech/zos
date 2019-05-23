da463342a1ccda7f21d0636140131274d0c6a99b
Christophe de Carvalho <christophe.dcpm@gmail.com>
2019-05-23 13:54:42 +0200

    modules: add a Method to get an Flister

---
+++ modules/client.go
@@ -0,0 +1,23 @@
+package modules
+
+import (
+	"github.com/threefoldtech/zbus"
+	"github.com/threefoldtech/zosv2/modules/stubs"
+)
+
+const redisSocket = "/var/run/redis.sock"
+
+var zbusClient zbus.Client
+
+func init() {
+	var err error
+	zbusClient, err = zbus.NewRedisClient(redisSocket)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Flist returns a client to the flist module
+func Flist() (Flister, error) {
+	return stubs.NewFlisterStub(zbusClient), nil
+}
