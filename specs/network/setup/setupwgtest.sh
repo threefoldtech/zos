f86e39a7dbc1018341b399d58eb5f6f1d2d6e5eb
Jan De Landtsheer <jan@delandtsheer.eu>
2019-06-18 14:35:42 +0200

    added config examples.. They're autogenerated, but good as reference

--- specs/network/setup/setupwgtest.sh
+++ specs/network/setup/setupwgtest.sh
@@ -15,25 +15,38 @@ function genconf() {
 		cat <<EOF >wg${i}.conf
 # WG${i}
 [Interface]
-ListenPort = 1234${i}
+ListenPort = ${port}
 PrivateKey = $PRIV
 EOF
 
 		for wg in $(seq 1 $NUM); do
 			if [ "$wg" -ne "$i" ]; then
+				port=$((16000 + $wg))
 				PUB=$(cat wg${wg}.pub)
+				h=$(printf '%x' $wg)
 				cat <<EEOF >>wg${i}.conf
 
 # Config for --- WG${wg} ---
 [Peer]
 PublicKey = $PUB
-Endpoint = 127.0.0.1:1234${wg}
-AllowedIPs = fe80::${wg}/128, 192.168.255.${wg}, ::/0
-PersistentKeepalive = 20
+Endpoint = 127.0.0.1:${port}
 EEOF
+				if [ "$wg" -eq "1" ]; then
+					cat <<EEEOF >>wg${i}.conf
+AllowedIPs = fe80::${h},192.168.255.${wg},2001:1:1:${h}::/64,::/0,0.0.0.0/0
+EEEOF
+				else
+					cat <<EEEOF >>wg${i}.conf
+AllowedIPs = fe80::${h},192.168.255.${wg},2001:1:1:${h}::/64
+EEEOF
+				fi
+				cat <<EEEOF >>wg${i}.conf
+PersistentKeepalive = 20
+EEEOF
 			fi
 		done
 	done
+	echo
 }
 
 function ns() {
@@ -53,11 +66,45 @@ function ns() {
 
 		ip netns exec wg${i} wg setconf wg${i} wg${i}.conf
 
-		ip -n wg${i} addr add fe80::${i}/64 dev wg${i}
-		ip -n wg0 addr add 192.168.255.${i}/24 dev wg${i}
+		ip -n wg${i} addr add fe80::${h}/64 dev wg${i}
+		ip -n wg${i} addr add 192.168.255.${i}/24 dev wg${i}
 
-		ip -n wg0 addr add 2001:1:1:${i}::1/64 dev int${i}
+		ip -n wg${i} addr add 2001:1:1:${h}::1/64 dev int${i}
+
 	done
+	echo
+}
+
+function addroutes() {
+
+	for i in $(seq 1 $NUM); do
+		echo -n "${i}.."
+		for wg in $(seq 1 $NUM); do
+			if [ "$wg" -ne "$i" ]; then
+				h=$(printf '%x' $wg)
+				# echo ip -n wg${i} route add 2001:1:1:${h}::/64 via fe80::${h} dev wg${i}
+				ip -n wg${i} route add 2001:1:1:${h}::/64 via fe80::${h} dev wg${i}
+			fi
+		done
+		# default route is via wg1
+		[ "$i" -ne "1" ] && ip -n wg${i} route add default via fe80::1 dev wg${i}
+	done
+}
+
+function exitNR() {
+	# the first is the exit NR
+	ip link add exit1 type veth peer name exit1tobr
+	ip link set exit1 netns wg1
+	ip -n wg1 link set exit1 up
+	ip link set exit1tobr up
+	ip netns exec wg1 setconf wg1 wg1.conf
+}
+function deleteall() {
+	for i in $(seq 1 $NUM); do
+		ip netns del wg${i}
+	done
+	rm -f wg*
+
 }
 
 function main() {
