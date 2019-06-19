5a83a23740e1ff39477088571a80d3a6f155bc9b
Christophe de Carvalho <christophe.dcpm@gmail.com>
2019-06-19 11:36:32 +0200

    Logic to apply network resource

--- modules/network/bridge/bridge_test.go
+++ modules/network/bridge/bridge_test.go
@@ -30,18 +30,42 @@ func TestCreateBridge(t *testing.T) {
 	assert.True(t, found)
 }
 
+func TestDeleteBridge(t *testing.T) {
+	err := Delete("notexits")
+	assert.NoError(t, err)
+
+	const ifName = "bro0"
+	br, err := New(ifName)
+	require.NoError(t, err)
+
+	// ensure bridge now exists
+	link, err := netlink.LinkByName(br.LinkAttrs.Name)
+	require.NoError(t, err)
+	_, ok := link.(*netlink.Bridge)
+	assert.True(t, ok)
+
+	// delete it
+	err = Delete(ifName)
+	assert.NoError(t, err)
+
+	// ensure bridge now it's gone
+	_, err = netlink.LinkByName(br.LinkAttrs.Name)
+	require.Error(t, err)
+}
+
 func TestAttachBridge(t *testing.T) {
 	const ifName = "bro0"
 	br, err := New(ifName)
 	require.NoError(t, err)
 
-	defer func() {
-		netlink.LinkDel(br)
-	}()
-
 	dummy := &netlink.Dummy{LinkAttrs: netlink.LinkAttrs{Name: "dummy0"}}
 	err = netlink.LinkAdd(dummy)
 	require.NoError(t, err)
+
+	defer func() {
+		_ = netlink.LinkDel(br)
+		_ = netlink.LinkDel(dummy)
+	}()
 
 	err = AttachNic(dummy, br)
 	assert.NoError(t, err)
