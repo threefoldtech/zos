54965efab10ffa6bcdabbda385217a75ab15c5ca
Christophe de Carvalho <christophe.dcpm@gmail.com>
2019-06-20 12:56:28 +0200

    fix some tests and add doc strings

--- modules/network/ip/nibble.go
+++ modules/network/ip/nibble.go
@@ -5,11 +5,14 @@ import (
 	"net"
 )
 
+// Nibble is an helper struct used to generate
+// deterministic name based on an IP address
 type Nibble struct {
 	allocNr   int8
 	hexNibble string
 }
 
+// NewNibble create a new Nibble object
 func NewNibble(prefix net.IPNet, allocNr int8) *Nibble {
 
 	var b []byte
@@ -26,15 +29,22 @@ func NewNibble(prefix net.IPNet, allocNr int8) *Nibble {
 	}
 }
 
+// WiregardName return the deterministic wireguard name
 func (n *Nibble) WiregardName() string {
 	return fmt.Sprintf("wg-%s-%d", n.hexNibble, n.allocNr)
 }
+
+// BridgeName return the deterministic bridge name
 func (n *Nibble) BridgeName() string {
 	return fmt.Sprintf("br-%s-%d", n.hexNibble, n.allocNr)
 }
+
+// NetworkName return the deterministic network name
 func (n *Nibble) NetworkName() string {
 	return fmt.Sprintf("net-%s-%d", n.hexNibble, n.allocNr)
 }
+
+// VethName return the deterministic veth interface name
 func (n *Nibble) VethName() string {
 	return fmt.Sprintf("veth-%s-%d", n.hexNibble, n.allocNr)
 }
