07916a81c067aa0049946693cc48acb3479c59db
Muhamad Azmy <muhamad.azmy+git@gmail.com>
2019-06-26 12:46:28 +0200

    Add some sources, and finalize the daemon entry

--- cmds/provisiond/main.go
+++ cmds/provisiond/main.go
@@ -19,9 +19,11 @@ func main() {
 
 	var (
 		msgBrokerCon string
+		resURL       string
 	)
 
 	flag.StringVar(&msgBrokerCon, "broker", "unix:///var/run/redis.sock", "connection string to the message broker")
+	flag.StringVar(&resURL, "url", "", "reservation url to poll from")
 
 	flag.Parse()
 
@@ -30,7 +32,20 @@ func main() {
 		log.Fatal().Msgf("fail to connect to message broker server: %v", err)
 	}
 
-	engine := provision.New(client, nil)
+	pipe, err := provision.FifoSource("/var/run/reservation.pipe")
+	if err != nil {
+		log.Fatal().Err(err).Msgf("failed to allocation reservation pipe")
+	}
+
+	source := pipe
+	if len(resURL) != 0 {
+		source = provision.CompinedSource(
+			pipe,
+			provision.HTTPSource(resURL),
+		)
+	}
+
+	engine := provision.New(client, source)
 
 	log.Info().
 		Str("broker", msgBrokerCon).
