89edb895a10fe121bd94dd6cd2c6990a82e25bf9
Christophe de Carvalho <christophe.dcpm@gmail.com>
2019-07-03 10:11:50 +0200

    set json tag to network structs

--- modules/network.go
+++ modules/network.go
@@ -42,16 +42,16 @@ const (
 
 // NodeID is a type defining a node ID
 type NodeID struct {
-	ID string
+	ID string `json:"id"`
 	// FarmeerID is needed for when a Node is HIDDEN, but lives in the same farm.
 	// that way if a network resource is started on a HIDDEN Node, and the peer
 	// is also HIDDEN, but part of the same farm, we can surmise that that peer
 	// can be included for that network resource
 	// https://www.wireguard.com/protocol/ -> we could send a handshake request
 	// to a HIDDEN peer and in case we receive a reply, include the peer in the list
-	FarmerID       string
-	ReachabilityV4 ReachabilityV4
-	ReachabilityV6 ReachabilityV6
+	FarmerID       string         `json:"farmer_id"`
+	ReachabilityV4 ReachabilityV4 `json:"reachability_v4"`
+	ReachabilityV6 ReachabilityV6 `json:"reachability_v6"`
 }
 
 // Network represent a full network owned by a user
@@ -60,15 +60,15 @@ type Network struct {
 	// that netid is bound to a user and an allowed (bought) creation of a
 	// node-local prefix for a bridge/container/vm
 	// needs to be queried from somewhere(TBD) to be filled in
-	NetID NetID `json":"network_id"`
+	NetID NetID `json:"network_id"`
 	// a netresource is a group of interconnected prefixes for a netid
 	// needs to be queried and updated when the netresource is created
-	Resources []*NetResource `json":"resources"`
+	Resources []*NetResource `json:"resources"`
 	// the exit is the ultimate default gateway container
 	// as well the prefix as the local config needs to be queried.
 	// - the prefix from the grid
 	// - the exit prefix and default gw from the local allocation
-	Exit *ExitPoint `json":"exit_point"`
+	Exit *ExitPoint `json:"exit_point"`
 	// AllocationNr is for when a new allocation has been necessary and needs to
 	// be added to the pool for Prefix allocations.
 	// this is needed as we set up deterministic interface names, that could conflict with
@@ -79,21 +79,21 @@ type Network struct {
 // NetResource represent a part of a network configuration
 type NetResource struct {
 	// where does it live
-	NodeID NodeID
+	NodeID *NodeID `json:"node_id"`
 	// prefix is the IPv6 allocation that will be connected to the
 	// bridge/container/vm
-	Prefix *net.IPNet
+	Prefix *net.IPNet `json:"prefix"`
 	// Gateways in IPv6 are link-local. To be able to use IPv6 in any way,
 	// an interface needs an IPv6 link-local address. As wireguard interfaces
 	// are l3-only, the kernel doesn't assign one, so we need to assign one
 	// ourselves. (we need to come up with a deterministic way, so we can be
 	// sure we now which/where)
-	LinkLocal *net.IPNet
+	LinkLocal *net.IPNet `json:"link_local"`
 	// what are the peers:
 	// each netresource needs to know what prefixes are reachable through
 	// what endpoint. Basically this `peers` array will be used to build
 	// up the wireguard config in each netresource.
-	Peers []*Peer
+	Peers []*Peer `json:"peers"`
 	// a list of firewall rules to open access directly, IF that netresource
 	// would be directly routed (future)
 	// IPv6Allow []net.IPNet
@@ -108,45 +108,45 @@ type NetResource struct {
 // through tunnels (like nodes in ipv4-only networks or home networks)
 type ExitPoint struct {
 	// netresource is the same as on all other netresources of a tenant network
-	*NetResource
+	*NetResource `json:"net_resource"`
 	// the ultimate IPv{4,6} config of the exit container.
-	Ipv4Conf Ipv4Conf
-	Ipv4DNAT []DNAT
+	Ipv4Conf *Ipv4Conf `json:"ipv4_conf"`
+	Ipv4DNAT []*DNAT   `json:"ipv4_dnat"`
 
-	Ipv6Conf  Ipv6Conf
-	Ipv6Allow []net.IP
+	Ipv6Conf  *Ipv6Conf `json:"ipv6_conf"`
+	Ipv6Allow []net.IP  `json:"ipv6_allow"`
 }
 
 // DNAT represents an ipv4/6 portforwarding/firewalling
 type DNAT struct {
-	InternalIP   net.IP
-	InternalPort uint16
+	InternalIP   net.IP `json:"internal_ip"`
+	InternalPort uint16 `json:"internal_port"`
 
-	ExternalIP   net.IP
-	ExternalPort uint16
+	ExternalIP   net.IP `json:"external_ip"`
+	ExternalPort uint16 `json:"external_port"`
 
-	Protocol string
+	Protocol string `json:"protocol"`
 }
 
 //Ipv4Conf represents the the IPv4 configuration of an exit container
 type Ipv4Conf struct {
 	// cidr
-	CIDR    *net.IPNet
-	Gateway net.IP
-	Metric  uint32
+	CIDR    *net.IPNet `json:"cird"`
+	Gateway net.IP     `json:"gateway"`
+	Metric  uint32     `json:"metric"`
 	// deterministic name in function of the prefix and it's allocation
-	Iface string
+	Iface string `json:"iface"`
 	// TBD, we need to establish if we want fc00/7 (ULA) or rfc1918 networks
 	// to be NATed (6to4 and/or 66)
-	EnableNAT bool
+	EnableNAT bool `json:"enable_nat"`
 }
 
 //Ipv6Conf represents the the IPv6 configuration of an exit container
 type Ipv6Conf struct {
-	Addr    net.IP
-	Gateway net.IP
-	Metric  uint32
-	Iface   string
+	Addr    *net.IPNet `json:"addr"`
+	Gateway net.IP     `json:"gateway"`
+	Metric  uint32     `json:"metric"`
+	Iface   string     `json:"iface"`
 }
 
 // Peer is a peer for which we have a tunnel established and the
@@ -157,9 +157,9 @@ type Ipv6Conf struct {
 // efficient to set up a vxlan (multicast or with direct fdb entries), than
 // using wireguard tunnels (that can be seen in a later phase)
 type Peer struct {
-	Type       ConnType
-	Prefix     *net.IPNet
-	Connection Wireguard
+	Type       ConnType   `json:"type"`
+	Prefix     *net.IPNet `json:"prefix"`
+	Connection Wireguard  `json:"connection"`
 }
 
 // ConnType is an enum
@@ -176,12 +176,12 @@ const (
 // locally and stored locally.
 type Wireguard struct {
 	// TBD, a peer can be IPv6, IPv6-ll or IPv4
-	IP net.IP
+	IP net.IP `json:"ip"`
 	// Listen port of wireguard
-	Port uint16
+	Port uint16 `json:"port"`
 	// base64 encoded public key
 	// Key []byte
-	Key string
+	Key string `json:"key"`
 }
 
 // definition for later usage
