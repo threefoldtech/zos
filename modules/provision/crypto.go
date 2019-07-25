aa4944838937310a381178d53a8a41513bffe9b0
Christophe de Carvalho <christophe.dcpm@gmail.com>
2019-07-26 00:24:25 +0200

    provisiond: implement cryptography primitives for resousrce identification
    fixes #83

---
+++ modules/provision/crypto.go
@@ -0,0 +1,98 @@
+package provision
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+	"io"
+
+	"github.com/threefoldtech/zosv2/modules/crypto"
+	"golang.org/x/crypto/ed25519"
+)
+
+// Sign creates a signature from all the field of the reservation
+// object and fill the Signature field
+func (r *Reservation) Sign(privateKey ed25519.PrivateKey) error {
+	buf := &bytes.Buffer{}
+	_, err := buf.WriteString(r.ID)
+	if err != nil {
+		return err
+	}
+	_, err = buf.WriteString(r.Tenant.Identity())
+	if err != nil {
+		return err
+	}
+	_, err = buf.WriteString(string(r.Type))
+	if err != nil {
+		return err
+	}
+	_, err = buf.Write(r.Data)
+	if err != nil {
+		return err
+	}
+
+	signature, err := crypto.Sign(privateKey, buf.Bytes())
+	if err != nil {
+		return err
+	}
+	r.Signature = signature
+	return nil
+}
+
+// Verify verifies the signature of the reservation
+func Verify(r *Reservation) error {
+	buf := &bytes.Buffer{}
+	_, err := buf.WriteString(r.ID)
+	if err != nil {
+		return err
+	}
+	_, err = buf.WriteString(r.Tenant.Identity())
+	if err != nil {
+		return err
+	}
+	_, err = buf.WriteString(string(r.Type))
+	if err != nil {
+		return err
+	}
+	_, err = buf.Write(r.Data)
+	if err != nil {
+		return err
+	}
+
+	publicKey, err := crypto.KeyFromID(r.Tenant)
+	if err != nil {
+		return err
+	}
+
+	return crypto.Verify(publicKey, buf.Bytes(), r.Signature)
+}
+
+func Hash(r Reservation) ([]byte, error) {
+	h := sha256.New()
+	_, err := io.WriteString(h, r.ID)
+	if err != nil {
+		return nil, err
+	}
+	_, err = io.WriteString(h, r.Tenant.Identity())
+	if err != nil {
+		return nil, err
+	}
+	_, err = io.WriteString(h, string(r.Type))
+	if err != nil {
+		return nil, err
+	}
+	_, err = h.Write(r.Data)
+	if err != nil {
+		return nil, err
+	}
+
+	return h.Sum(nil), nil
+}
+
+func HexHash(r Reservation) (string, error) {
+	h, err := Hash(r)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%x", h), nil
+}
