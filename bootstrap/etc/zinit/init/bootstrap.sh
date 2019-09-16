da0f785a5b5a4c0dd371e0c9b2212fe11865830d
Muhamad Azmy <muhamad.azmy+git@gmail.com>
2019-09-16 12:34:45 +0200

    Update bootstrap default flist

--- bootstrap/etc/zinit/init/bootstrap.sh
+++ bootstrap/etc/zinit/init/bootstrap.sh
@@ -1,6 +1,6 @@
 set -x
 
-DEFAULT_FLIST=azmy/zos_ref_heads_master.flist
+DEFAULT_FLIST=azmy/zos-refs_heads_master.flist
 
 # bootflist reads the boot flist name from kernel cmd
 # TODO: this should probably be not allowed at somepoint to
