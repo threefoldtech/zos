1bb8372956c30f8be0f7cacb759fd97b08454e06
Christophe de Carvalho <christophe.dcpm@gmail.com>
2019-09-19 15:16:02 +0200

    enable flist unit tests

--- test_scripts/install_0-fs.sh
+++ test_scripts/install_0-fs.sh
@@ -2,8 +2,13 @@
 
 set -ex
 
+export GO11MODULE=off
+
+go get -v github.com/tools/godep
+
 mkdir -p /home/runner/work/zosv2/0-fs/
 cd  /home/runner/work/zosv2/0-fs/
 git clone --depth 1 https://github.com/threefoldtech/0-fs.git
 cd 0-fs
+godep restore
 make install
