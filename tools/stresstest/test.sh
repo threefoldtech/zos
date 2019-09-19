154f265b7685ed46e06011973d24ccd7de5418db
Maxime Daniel <root@maxux.net>
2019-09-19 11:19:45 +0200

    Update stresstest to latest changes

--- tools/stresstest/test.sh
+++ tools/stresstest/test.sh
@@ -4,7 +4,7 @@ set -ex
 tfubin="${PWD}/../tfuser/tfuser"
 schemas="${PWD}/schemas"
 
-farmid="CemYjciEmuvYVKDFXYaZLdGsCdLDRp4U1Xu1LPPrQNkK"
+farmid="A3y5F8CoHVZiq3SvtY9pcJXC67aotSPk8AKMZYzkxyb6"
 tnodb="https://tnodb.dev.grid.tf"
 
 redislog="10.4.0.250"
@@ -20,29 +20,36 @@ identity=$($tfubin id -o ${schemas}/user.seed | grep 'identity:' | awk '{ print 
 
 echo "[+] identity: ${identity}"
 
-echo "[+] fetching farms nodes list"
-nodesjson=$(curl -s "${tnodb}/nodes?farm=${farmid}" | python -m json.tool)
+echo "[+] fetching nodes list"
+nodesjson=$(curl -s "${tnodb}/nodes" | python -m json.tool)
+fnodesjson=$(curl -s "${tnodb}/nodes?farm=${farmid}" | python -m json.tool)
 
 echo "[+] selecting one exit node"
-node=$(echo "$nodesjson" | egrep 'exit_node|node_id' | grep -E -B1 ': [1-9]' | head -1 | awk -F'"' '{ print $4 }')
+exitnode=$(echo "$nodesjson" | egrep 'exit_node|node_id' | grep -E -B1 ': [1-9]' | head -1 | awk -F'"' '{ print $4 }')
 
-echo "[+] exit node selected: $node"
+echo "[+] exit node selected: $exitnode"
+
+echo "[+] selecting one node in the farm"
+node=$(echo "$fnodesjson" | egrep 'node_id' | awk -F'"' '{ print $4 }')
 
 echo "[+] creating a new network"
-$tfubin generate network create --node $node > ${schemas}/net-init.json
+$tfubin generate network create --node $exitnode > ${schemas}/net-init.json
 netid=$(cat ${schemas}/net-init.json | python -m json.tool | grep network_id | awk -F'"' '{ print $4 }')
 
-echo "[+] adding user to network"
-wgkey=$($tfubin generate --schema ${schemas}/net-init.json network add-user --user ${identity} | head -1 | awk '{ print $4 }')
+echo "[+] add the node into the network"
+$tfubin generate --schema ${schemas}/net-init.json network add-node --node $node
 
-echo "[+] generating wireguard config"
-$tfubin generate --schema ${schemas}/net-init.json network wg --user ${identity} --key ${wgkey} > ${schemas}/wg.conf
+# echo "[+] adding user to network"
+# wgkey=$($tfubin generate --schema ${schemas}/net-init.json network add-user --user ${identity} | head -1 | awk '{ print $4 }')
+
+# echo "[+] generating wireguard config"
+# $tfubin generate --schema ${schemas}/net-init.json network wg --user ${identity} --key ${wgkey} > ${schemas}/wg.conf
 
 echo "[+] generating debug mode (redis ${redislog} -> ${redischan})"
 $tfubin generate debug --endpoint "${redislog}:6379" --channel ${redischan} > ${schemas}/debug-node.json
 
 echo "[+] generating container"
-$tfubin container --flist https://hub.grid.tf/maxux/busybox-latest.flist --entrypoint /bin/ash --corex --network ${netid} --envs hello=world > ${schema}/busybox-corex.json
+$tfubin generate container --flist https://hub.grid.tf/maxux/busybox-latest.flist --entrypoint /bin/ash --corex --network ${netid} --envs hello=world > ${schemas}/busybox-corex.json
 
 echo "[+] generating zdb profiles"
 $tfubin generate storage zdb --size 10 --type SSD --mode user > ${schemas}/zdb-ssd-10.json
