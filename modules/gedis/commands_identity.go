f1bbfab5d229cbbd32d15499010ece56910e5350
Muhamad Azmy <muhamad.azmy+git@gmail.com>
2019-09-26 12:12:30 +0200

    Listing and getting node with decent types

--- modules/gedis/commands_identity.go
+++ modules/gedis/commands_identity.go
@@ -2,9 +2,10 @@ package gedis
 
 import (
 	"encoding/json"
-	"fmt"
+	"net"
 
 	"github.com/threefoldtech/zosv2/modules/gedis/types/directory"
+	"github.com/threefoldtech/zosv2/modules/network/types"
 
 	"github.com/threefoldtech/zosv2/modules"
 )
@@ -40,7 +41,7 @@ func (g *Gedis) RegisterNode(nodeID, farmID modules.Identifier, version string) 
 }
 
 // ListNode implements modules.IdentityManager interface
-func (g *Gedis) ListNode(farmID modules.Identifier, country string, city string) ([]directory.TfgridNode2, error) {
+func (g *Gedis) ListNode(farmID modules.Identifier, country string, city string) ([]types.Node, error) {
 	resp, err := Bytes(g.Send("nodes", "list", Args{
 		"farm_id": farmID.Identity(),
 		"country": country,
@@ -51,7 +52,6 @@ func (g *Gedis) ListNode(farmID modules.Identifier, country string, city string)
 		return nil, err
 	}
 
-	fmt.Println(string(resp))
 	var out struct {
 		Nodes []directory.TfgridNode2 `json:"nodes"`
 	}
@@ -60,7 +60,12 @@ func (g *Gedis) ListNode(farmID modules.Identifier, country string, city string)
 		return nil, err
 	}
 
-	return out.Nodes, nil
+	var result []types.Node
+	for _, node := range out.Nodes {
+		result = append(result, nodeFromSchema(node))
+	}
+
+	return result, nil
 }
 
 //RegisterFarm implements modules.IdentityManager interface
@@ -89,31 +94,60 @@ func (g *Gedis) RegisterFarm(farm modules.Identifier, name string, email string,
 	return out.FarmID.Int64()
 }
 
-// //GetNode implements modules.IdentityManager interface
-// func (g *Gedis) GetNode(nodeID modules.Identifier) (*types.Node, error) {
-// 	req := getNodeBody{
-// 		NodeID: nodeID.Identity(),
-// 	}
+//GetNode implements modules.IdentityManager interface
+func (g *Gedis) GetNode(nodeID modules.Identifier) (*types.Node, error) {
+	resp, err := Bytes(g.Send("nodes", "get", Args{
+		"node_id": nodeID.Identity(),
+	}))
 
-// 	b, err := json.Marshal(req)
-// 	if err != nil {
-// 		return nil, err
-// 	}
+	if err != nil {
+		return nil, err
+	}
 
-// 	resp, err := g.sendCommand("nodes", "get", b)
-// 	if err != nil {
-// 		return nil, parseError(err)
-// 	}
+	var out directory.TfgridNode2
 
-// 	n := tfgridNode2{}
-// 	if err := json.Unmarshal(resp, &n); err != nil {
-// 		return nil, err
-// 	}
-// 	return &types.Node{
-// 		NodeID: n.NodeID,
-// 		FarmID: n.FarmID,
-// 	}, nil
-// }
+	if err := json.Unmarshal(resp, &out); err != nil {
+		return nil, err
+	}
+
+	node := nodeFromSchema(out)
+	return &node, nil
+}
+
+func infFromSchema(inf directory.TfgridNodeIface1) types.IfaceInfo {
+	return types.IfaceInfo{
+		Name:    inf.Name,
+		Gateway: inf.Gateway,
+		Addrs: func() []*net.IPNet {
+			var r []*net.IPNet
+			for _, addr := range inf.Addrs {
+				r = append(r, &addr.IPNet)
+			}
+			return r
+		}(),
+	}
+}
+
+func nodeFromSchema(node directory.TfgridNode2) types.Node {
+	return types.Node{
+		NodeID: node.NodeId,
+		FarmID: node.FarmId,
+		Ifaces: func() []*types.IfaceInfo {
+			var r []*types.IfaceInfo
+			for _, iface := range node.Ifaces {
+				v := infFromSchema(iface)
+				r = append(r, &v)
+			}
+			return r
+		}(),
+		ExitNode: func() int {
+			if node.ExitNode {
+				return 1
+			}
+			return 0
+		}(),
+	}
+}
 
 // func (g *Gedis) updateGenericNodeCapacity(captype string, node modules.Identifier, mru int64, cru int64, hru int64, sru int64) error {
 // 	req := gedisNodeUpdateCapacity{
