719bcf5952370bea543adddc28f1fb540f48dc3d
Christophe de Carvalho <christophe.dcpm@gmail.com>
2019-09-27 13:20:46 +0200

    create geoip package

---
+++ modules/geoip/geoip.go
@@ -0,0 +1,41 @@
+package geoip
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+)
+
+type Location struct {
+	Longitute float64 `json:"longitude,omitempty"`
+	Latitude  float64 `json:"latitude,omitempty"`
+	Continent string  `json:"continent,omitempty"`
+	Country   string  `json:"country_name,omitempty"`
+	City      string  `json:"city,omitempty"`
+}
+
+func Fetch() (Location, error) {
+	l := Location{
+		Longitute: 0.0,
+		Latitude:  0.0,
+		Continent: "Uknown",
+		Country:   "Uknown",
+		City:      "Uknown",
+	}
+
+	resp, err := http.Get("https://geoip.grid.tf")
+	if err != nil {
+		return l, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return l, errors.New("error fetch location")
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(&l); err != nil {
+		return l, err
+	}
+
+	return l, err
+}
