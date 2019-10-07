b596ce236d328689361965283ebd22f01a987e9a
Muhamad Azmy <muhamad.azmy@codescalers.com>
2019-10-07 15:01:13 +0200

    finish testing gedis

--- pkg/gedis/commands_network_test.go
+++ pkg/gedis/commands_network_test.go
@@ -38,9 +38,7 @@ func TestNetworkPublishInterfaces(t *testing.T) {
 	}
 
 	conn.On("Do", "default.nodes.publish_interfaces", mustMarshal(t, args)).
-		Return(mustMarshal(t, directory.TfgridNode2{
-			NodeID: "node-1",
-		}), nil)
+		Return(nil, nil)
 
 	inf := types.IfaceInfo{
 		Name: "eth0",
@@ -50,6 +48,39 @@ func TestNetworkPublishInterfaces(t *testing.T) {
 		Gateway: []net.IP{net.ParseIP("192.168.1.1")},
 	}
 	err := gedis.PublishInterfaces(id, []types.IfaceInfo{inf})
+
+	require.NoError(err)
+	conn.AssertCalled(t, "Close")
+}
+
+func TestNetworkSetPublicIface(t *testing.T) {
+	require := require.New(t)
+	pool, conn := getTestPool()
+	gedis := Gedis{
+		pool:      pool,
+		namespace: "default",
+	}
+
+	id := pkg.StrIdentifier("node-1")
+	r := schema.MustParseIPRange("192.168.1.2/24")
+	args := Args{
+		"node_id": id,
+		"public": directory.TfgridNodePublicIface1{
+			Master: "eth0",
+			Ipv4:   r,
+			Gw4:    net.ParseIP("192.168.1.1"),
+		},
+	}
+
+	conn.On("Do", "default.nodes.set_public_iface", mustMarshal(t, args)).
+		Return(nil, nil)
+
+	err := gedis.SetPublicIface(id, &types.PubIface{
+		Master: "eth0",
+		Type:   types.MacVlanIface,
+		IPv4:   &r.IPNet,
+		GW4:    net.ParseIP("192.168.1.1"),
+	})
 
 	require.NoError(err)
 	conn.AssertCalled(t, "Close")
