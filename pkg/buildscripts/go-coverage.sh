11e7dcab589e4ca13b3d61e241844909970f09d2
Muhamad Azmy <muhamad.azmy@codescalers.com>
2019-10-07 15:31:58 +0200

    enable storage

--- pkg/buildscripts/go-coverage.sh
+++ pkg/buildscripts/go-coverage.sh
@@ -3,7 +3,7 @@
 set -e
 echo "" > coverage.txt
 
-for d in $(go list ./... | grep -Ev "stubs|provision|network|storage" ); do
+for d in $(go list ./... | grep -Ev "stubs|provision|network" ); do
     echo "test $d"
     go test -vet=off -coverprofile=profile.out -race -covermode=atomic "$d"
     if [ -f profile.out ]; then
