5b1ec33ae32791ca89460e70e6e4e2cab710268f
Muhamad Azamy <muhamad@incubaid.com>
2019-12-10 13:19:13 +0100

    Use readdir instead of walk
    In this use case readdir is much faster

--- pkg/storage/zdbpool/pool.go
+++ pkg/storage/zdbpool/pool.go
@@ -1,6 +1,7 @@
 package zdbpool
 
 import (
+	"io/ioutil"
 	"os"
 	"path/filepath"
 
@@ -16,6 +17,7 @@ type ZDBPool struct {
 	path string
 }
 
+// New creates a ZDBPool with path
 func New(path string) ZDBPool {
 	return ZDBPool{path}
 }
@@ -28,7 +30,7 @@ type NSInfo struct {
 
 // Reserved return the amount of storage that has been reserved by all the
 // namespace in the pool
-func (p ZDBPool) Reserved() (uint64, error) {
+func (p *ZDBPool) Reserved() (uint64, error) {
 	ns, err := p.Namespaces()
 	if err != nil {
 		return 0, err
@@ -42,48 +44,63 @@ func (p ZDBPool) Reserved() (uint64, error) {
 	return total, nil
 }
 
+// Namespace gets a namespace info from pool
+func (p *ZDBPool) Namespace(name string) (info NSInfo, err error) {
+	path := filepath.Join(p.path, name, "zdb-namespace")
+	f, err := os.Open(path)
+	if err != nil {
+		return info, err
+	}
+	defer f.Close()
+	var header Header
+	if err := ReadHeader(f, &header); err != nil {
+		return info, errors.Wrapf(err, "failed to read namespace header at %s", path)
+	}
+
+	info = NSInfo{
+		Name: name,
+		Size: header.MaxSize,
+	}
+
+	return
+}
+
 // Namespaces returns a list of NSinfo of all the namespace present in the pool
-func (p ZDBPool) Namespaces() ([]NSInfo, error) {
-	root := p.path
-	nsinfos := make([]NSInfo, 0)
-	header := Header{}
+func (p *ZDBPool) Namespaces() ([]NSInfo, error) {
 
-	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-		if path == root {
-			return nil
+	dirs, err := ioutil.ReadDir(p.path)
+	if err != nil {
+		return nil, err
+	}
+
+	var namespaces []NSInfo
+	for _, dir := range dirs {
+		if !dir.IsDir() {
+			continue
 		}
 
-		// skip default namespace
-		if info.Name() == "default" {
-			return filepath.SkipDir
+		if dir.Name() == "default" {
+			continue
 		}
 
-		hp := filepath.Join(path, "zdb-namespace")
-		f, err := os.Open(hp)
-		if err != nil {
-			return errors.Wrapf(err, "failed to open namespace header at %s", path)
-		}
-		defer f.Close()
-
-		if err := ReadHeader(f, &header); err != nil {
-			return errors.Wrapf(err, "failed to read namespace header at %s", path)
+		info, err := p.Namespace(dir.Name())
+		if os.IsNotExist(err) {
+			// not a valid namespace directory
+			continue
+		} else if err != nil {
+			return nil, err
 		}
 
-		nsinfos = append(nsinfos, NSInfo{
-			Name: info.Name(),
-			Size: header.MaxSize,
-		})
+		namespaces = append(namespaces, info)
+	}
 
-		return filepath.SkipDir
-	})
-
-	return nsinfos, err
+	return namespaces, nil
 }
 
 // Exists checks if a namespace exists in the pool or not
 // this method is way faster then using Namespaces cause it doesn't have to read any data
-func (p ZDBPool) Exists(name string) bool {
-	path := filepath.Join(p.path, name)
+func (p *ZDBPool) Exists(name string) bool {
+	path := filepath.Join(p.path, name, "zdb-namespace")
 
 	_, err := os.Stat(path)
 	return err == nil
