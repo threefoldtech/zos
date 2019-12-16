02a80c4861d2e1d59954e1e87c19ac38b5c33411
Muhamad Azamy <muhamad@incubaid.com>
2019-12-16 10:27:44 +0100

    Fix tests

--- pkg/gedis/commands_provision_test.go
+++ pkg/gedis/commands_provision_test.go
@@ -30,7 +30,7 @@ func TestProvisionGet(t *testing.T) {
 
 	workload := `{"workload_id": 10, "type": 0, "size": 100}`
 
-	conn.On("Do", "tfgrid.directory.workload_manager.workload_get", mustMarshal(t, args)).
+	conn.On("Do", "tfgrid.workloads.workload_manager.workload_get", mustMarshal(t, args)).
 		Return(mustMarshal(t, Args{
 			"workload_id": id,
 			"type":        types.TfgridReservationWorkload1TypeVolume,
@@ -63,7 +63,7 @@ func TestProvisionPoll(t *testing.T) {
 	workloadVol := `{"workload_id": 1, "type": 0, "size": 100}`
 	workloadZdb := `{"workload_id": 1, "mode": 0, "size": 100}`
 
-	conn.On("Do", "tfgrid.directory.workload_manager.workloads_list", mustMarshal(t, args)).
+	conn.On("Do", "tfgrid.workloads.workload_manager.workloads_list", mustMarshal(t, args)).
 		Return(mustMarshal(t, Args{
 			"workloads": []types.TfgridReservationWorkload1{
 				{
@@ -93,7 +93,7 @@ func TestProvisionPoll(t *testing.T) {
 		"cursor":  10,
 	}
 
-	conn.On("Do", "tfgrid.directory.workload_manager.workloads_list", mustMarshal(t, args)).
+	conn.On("Do", "tfgrid.workloads.workload_manager.workloads_list", mustMarshal(t, args)).
 		Return(mustMarshal(t, Args{
 			"workloads": []types.TfgridReservationWorkload1{
 				{
@@ -148,7 +148,7 @@ func TestProvisionFeedback(t *testing.T) {
 		},
 	}
 
-	conn.On("Do", "tfgrid.directory.workload_manager.set_workload_result", mustMarshal(t, args)).
+	conn.On("Do", "tfgrid.workloads.workload_manager.set_workload_result", mustMarshal(t, args)).
 		Return(nil, nil)
 
 	err := gedis.Feedback(id, &result)
@@ -197,7 +197,7 @@ func TestProvisionReserve(t *testing.T) {
 		},
 	}
 
-	conn.On("Do", "tfgrid.directory.workload_manager.reservation_register", mock.MatchedBy(func(in []byte) bool {
+	conn.On("Do", "tfgrid.workloads.workload_manager.reservation_register", mock.MatchedBy(func(in []byte) bool {
 		EqualJSON(t, mustMarshal(t, args), in)
 		return true
 	})).
