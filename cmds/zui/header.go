5969b26a26700506fdef4380fca1a1fd3f3162c5
Muhamad Azamy <muhamad@incubaid.com>
2019-12-24 14:21:18 +0100

    Show running mode

--- cmds/zui/header.go
+++ cmds/zui/header.go
@@ -7,10 +7,14 @@ import (
 	"github.com/gizak/termui/v3/widgets"
 	"github.com/rs/zerolog/log"
 	"github.com/threefoldtech/zbus"
+	"github.com/threefoldtech/zos/pkg/environment"
 	"github.com/threefoldtech/zos/pkg/stubs"
 )
 
 func headerRenderer(c zbus.Client, h *widgets.Paragraph, r func()) {
+	env, _ := environment.Get()
+	format := fmt.Sprintf("Zero OS [%s] Version: %%s", env.RunningMode.String())
+
 	h.Text = "Zero OS"
 
 	host := stubs.NewHostMonitorStub(c)
@@ -22,7 +26,7 @@ func headerRenderer(c zbus.Client, h *widgets.Paragraph, r func()) {
 
 	go func() {
 		for version := range ch {
-			v := fmt.Sprintf("Zero OS (%s)", version.String())
+			v := fmt.Sprintf(format, version.String())
 			if h.Text != v {
 				h.Text = v
 				r()
