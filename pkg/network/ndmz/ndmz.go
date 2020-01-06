bd4497171c1fb51aa7837b1fcfc6d6733f236413
Jan De Landtsheer <jan@delandtsheer.eu>
2020-01-06 17:29:44 +0100

    added tonrs back.
    - ergo: another setupvethwithname bites the dust
    - rename createMacVlan function to createPublicIface to be more explicit
    - ensure tonrs is up
    
    fixes #466

--- pkg/network/ndmz/ndmz.go
+++ pkg/network/ndmz/ndmz.go
@@ -21,7 +21,6 @@ import (
 	"github.com/rs/zerolog/log"
 	"github.com/vishvananda/netlink"
 
-	"github.com/containernetworking/plugins/pkg/ip"
 	"github.com/containernetworking/plugins/pkg/ns"
 	"github.com/containernetworking/plugins/pkg/utils/sysctl"
 	"github.com/pkg/errors"
@@ -67,7 +66,7 @@ func Create(nodeID pkg.Identifier) error {
 		return err
 	}
 
-	if err := createMacVlan(netNS); err != nil {
+	if err := createPublicIface(netNS); err != nil {
 		return err
 	}
 
@@ -111,8 +110,8 @@ func Delete() error {
 	return nil
 }
 
-func createMacVlan(netNS ns.NetNS) error {
-	if !macvlan.Exists(types.PublicIface, netNS) {
+func createPublicIface(netNS ns.NetNS) error {
+	if !ifaceutil.Exists(types.PublicIface, netNS) {
 
 		var (
 			master netlink.Link
@@ -136,66 +135,45 @@ func createMacVlan(netNS ns.NetNS) error {
 }
 
 func createRoutingBridge(netNS ns.NetNS) error {
-	if bridge.Exists(BridgeNDMZ) && namespace.Exists(netNSNDMZ) {
-		return nil
-	}
-
-	var (
-		br  *netlink.Bridge
-		err error
-	)
-
 	if !bridge.Exists(BridgeNDMZ) {
-		br, err = bridge.New(BridgeNDMZ)
-		if err != nil {
+		if _, err := bridge.New(BridgeNDMZ); err != nil {
 			return err
 		}
 	}
 
-	vethNDMZ := "tonrs"
-	vethHost := "br-tonrs"
+	const tonrsIface = "tonrs"
 
-	if _, _, err = ip.SetupVethWithName(vethHost, vethNDMZ, 1500, netNS); err != nil {
-		return errors.Wrap(err, "failed to create veth pair for ndmz")
+	if !ifaceutil.Exists(tonrsIface, netNS) {
+		if _, err := macvlan.Create(tonrsIface, BridgeNDMZ, netNS); err != nil {
+			return err
+		}
 	}
-	log.Info().
-		Str("ndmz side", vethNDMZ).
-		Str("host side", vethHost).
-		Msg("veth pair for ndmz bridge created")
 
 	if _, err := sysctl.Sysctl(fmt.Sprintf("net.ipv6.conf.%s.disable_ipv6", BridgeNDMZ), "1"); err != nil {
 		return errors.Wrapf(err, "failed to disable ip6 on bridge %s", BridgeNDMZ)
 	}
 
-	if _, err := sysctl.Sysctl(fmt.Sprintf("net.ipv6.conf.%s.disable_ipv6", vethHost), "1"); err != nil {
-		return errors.Wrapf(err, "failed to disable ip6 on interface %s", vethHost)
-	}
-
-	lVethBr, err := netlink.LinkByName(vethHost)
-	if err != nil {
-		return err
-	}
-
-	if err := bridge.AttachNic(lVethBr, br); err != nil {
-		return err
-	}
-
 	return netNS.Do(func(_ ns.NetNS) error {
-		if _, err := sysctl.Sysctl(fmt.Sprintf("net.ipv6.conf.%s.disable_ipv6", vethNDMZ), "1"); err != nil {
-			return errors.Wrapf(err, "failed to disable ip6 on veth pair %s", vethNDMZ)
+		if _, err := sysctl.Sysctl(fmt.Sprintf("net.ipv6.conf.%s.disable_ipv6", tonrsIface), "1"); err != nil {
+			return errors.Wrapf(err, "failed to disable ip6 on macvlan %s", tonrsIface)
 		}
 
-		lVethGW, err := netlink.LinkByName(vethNDMZ)
+		link, err := netlink.LinkByName(tonrsIface)
 		if err != nil {
 			return err
 		}
 
-		return netlink.AddrAdd(lVethGW, &netlink.Addr{
+		err = netlink.AddrAdd(link, &netlink.Addr{
 			IPNet: &net.IPNet{
 				IP:   net.ParseIP("100.127.0.1"),
 				Mask: net.CIDRMask(16, 32),
 			},
 		})
+		if err != nil && !os.IsExist(err) {
+			return err
+		}
+
+		return netlink.LinkSetUp(link)
 	})
 }
 
@@ -261,8 +239,6 @@ func AttachNR(networkID string, nr *nr.NetResource) error {
 		if err != nil && !os.IsExist(err) {
 			return err
 		}
-
 		return nil
 	})
-
 }
