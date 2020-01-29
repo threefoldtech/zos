80f36f2de88f0fd821fbab3b8b8c6f84c9aeeb5d
Maxime Daniel <root@maxux.net>
2020-01-29 15:34:05 +0100

    Add autobuild scripts

---
+++ bins/autobuild/tf-build-deps.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# install dependencies for building
+apt-get update
+
+# toolchain dependencies
+deps=(pkg-config make m4 autoconf)
+
+# system tools and libs
+deps+=(libssl-dev dnsmasq git curl bc wget unzip)
+
+# storage and filesystem
+deps+=(e2fslibs-dev libblkid-dev uuid-dev libattr1-dev btrfs-tools)
+
+apt-get install -y ${deps[@]}
+
+# install go
+curl -L https://dl.google.com/go/go1.13.1.linux-amd64.tar.gz > /tmp/go1.13.1.linux-amd64.tar.gz
+tar -C /usr/local -xzf /tmp/go1.13.1.linux-amd64.tar.gz
+mkdir -p /gopath
+
+# install rust
+curl https://sh.rustup.rs -sSf | sh -s -- -y
---
+++ bins/autobuild/tf-build-settings.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# setting variables
+export PATH=$PATH:/usr/local/go/bin
+export GOPATH=/gopath
+source $HOME/.cargo/env
