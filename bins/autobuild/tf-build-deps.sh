46da7a274c6c18afa93026450b34a1747dc1cb7f
Maxime Daniel <root@maxux.net>
2020-01-29 15:57:14 +0100

    Disable go and rust from autobuild script

--- bins/autobuild/tf-build-deps.sh
+++ bins/autobuild/tf-build-deps.sh
@@ -15,9 +15,9 @@ deps+=(e2fslibs-dev libblkid-dev uuid-dev libattr1-dev btrfs-tools)
 apt-get install -y ${deps[@]}
 
 # install go
-curl -L https://dl.google.com/go/go1.13.1.linux-amd64.tar.gz > /tmp/go1.13.1.linux-amd64.tar.gz
-tar -C /usr/local -xzf /tmp/go1.13.1.linux-amd64.tar.gz
-mkdir -p /gopath
+## curl -L https://dl.google.com/go/go1.13.1.linux-amd64.tar.gz > /tmp/go1.13.1.linux-amd64.tar.gz
+## tar -C /usr/local -xzf /tmp/go1.13.1.linux-amd64.tar.gz
+## mkdir -p /gopath
 
 # install rust
-curl https://sh.rustup.rs -sSf | sh -s -- -y
+## curl https://sh.rustup.rs -sSf | sh -s -- -y
