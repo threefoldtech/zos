f77f78aa3bc98e7940eec47d0cee1b963b9d883e
Muhamad Azamy <muhamad@incubaid.com>
2020-01-29 16:47:14 +0200

    create symlink with exact target

--- pkg/upgrade/upgrade.go
+++ pkg/upgrade/upgrade.go
@@ -383,13 +383,12 @@ func copyRecursive(source string, destination string, skip ...string) error {
 				return err
 			}
 		} else if (info.Mode() & os.ModeSymlink) == os.ModeSymlink {
-			ldest, err := os.Readlink(path)
+			target, err := os.Readlink(path)
 			if err != nil {
 				return err
 			}
-
-			src := filepath.Join(destination, ldest)
-			if err := os.Symlink(dest, src); err != nil && !os.IsExist(err) {
+			log.Debug().Str("link", dest).Str("target", target).Msg("Linking")
+			if err := os.Symlink(dest, target); err != nil && !os.IsExist(err) {
 				return err
 			}
 		}
