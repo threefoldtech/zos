b3b840f402500815e58c03e409d812c0ef05fd1b
maxime.vanhees <maxime.vanhees@student.odisee>
2020-03-05 14:17:46 +0100

    added flag support

--- pkg/app/flag.go
+++ pkg/app/flag.go
@@ -1,8 +1,42 @@
 package app
 
-/*
-const LimitedCache = "limited-cache"
+import (
+	"os"
+	"path/filepath"
 
-func setFlag() error {
+	"github.com/pkg/errors"
+)
+
+const (
+	flagsDir     = "/tmp/flags"
+	LimitedCache = "limited-cache"
+)
+
+// MakeFlag is used when the /var/cache cannot be mounted on a SSD or HDD,
+// it will mount the cache disk on a temporary file system in memory.
+func SetFlag(key string) error {
+	// creating a file that will be used as a flag
+	// the flag can 'warn' other deamons that the /var/cache is not on HDD or SDD
+	if err := os.MkdirAll(flagsDir, os.ModePerm); err != nil {
+		return errors.Wrap(err, "failed to create flags directory")
+	}
+
+	f, err := os.Create(filepath.Join(flagsDir, key))
+	if err != nil {
+		return errors.Wrap(err, "failed to create the flag file")
+	}
+
+	return f.Close()
+}
+
+func ClearFlag(key string) error {
+	if err := os.Remove(filepath.Join(flagsDir, key)); err != nil {
+		return errors.Wrap(err, "failed to remove flag file")
+	}
 	return nil
-} */
+}
+
+func CheckFlag(key string) bool {
+	_, err := os.Stat(filepath.Join(flagsDir, key))
+	return os.IsNotExist(err)
+}
