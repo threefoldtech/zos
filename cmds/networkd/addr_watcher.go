ea4a418459dd9a3dc21145d55fd2826ab20745ad
Christophe de Carvalho <christophe.dcpm@gmail.com>
2020-04-02 18:46:01 +0200

    networkd: limit the amount of address update send to the explorer to 1 every 10 minutes
    fixes #583

--- cmds/networkd/addr_watcher.go
+++ cmds/networkd/addr_watcher.go
@@ -13,8 +13,8 @@ import (
 	"github.com/threefoldtech/zos/pkg/network/ifaceutil"
 	"github.com/threefoldtech/zos/pkg/network/types"
 	"github.com/threefoldtech/zos/pkg/schema"
-	"github.com/threefoldtech/zos/tools/explorer/models/generated/directory"
 	"github.com/threefoldtech/zos/tools/client"
+	"github.com/threefoldtech/zos/tools/explorer/models/generated/directory"
 	"github.com/vishvananda/netlink"
 )
 
@@ -69,6 +69,8 @@ func (w WatchedLinks) Forever(ctx context.Context) error {
 		return err
 	}
 
+	nextAllowed := time.Now()
+
 	for {
 		select {
 
@@ -81,9 +83,14 @@ func (w WatchedLinks) Forever(ctx context.Context) error {
 				return fmt.Errorf("netlink closed the subscription channel")
 			}
 
-			log.Debug().Msgf("addr update received %+v", update)
-			if err := w.callBack(update); err != nil {
-				log.Error().Err(err).Msg("addr watcher: error during callback")
+			now := time.Now()
+			if now.After(nextAllowed) {
+				log.Debug().Msgf("addr update received %+v", update)
+
+				if err := w.callBack(update); err != nil {
+					log.Error().Err(err).Msg("addr watcher: error during callback")
+				}
+				nextAllowed = now.Add(time.Minute * 10)
 			}
 		}
 	}
