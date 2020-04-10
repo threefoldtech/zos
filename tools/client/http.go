ad7238a47271bfdf93c953599a28fe7be8d1a9b5
Christophe de Carvalho <christophe.dcpm@gmail.com>
2020-04-10 13:04:51 +0200

    tffarmer: add email and IYO organization flag to farm create command

--- tools/client/http.go
+++ tools/client/http.go
@@ -28,6 +28,22 @@ type httpClient struct {
 	identity string
 }
 
+// HTTPError is the error type returned by the client
+// it contains the error and the HTTP response
+type HTTPError struct {
+	resp *http.Response
+	err  error
+}
+
+func (h HTTPError) Error() string {
+	return fmt.Sprintf("%v status:%s", h.err, h.resp.Status)
+}
+
+// Response return the HTTP response that trigger this error
+func (h HTTPError) Response() http.Response {
+	return *h.resp
+}
+
 func newHTTPClient(raw string, kp identity.KeyPair) (*httpClient, error) {
 	u, err := url.Parse(raw)
 	if err != nil {
@@ -82,7 +98,10 @@ func (c *httpClient) process(response *http.Response, output interface{}, expect
 			return errors.Wrapf(err, "failed to load error while processing invalid return code of: %s", response.Status)
 		}
 
-		return fmt.Errorf("%s: %s", response.Status, output.E)
+		return HTTPError{
+			err:  fmt.Errorf(output.E),
+			resp: response,
+		}
 	}
 
 	if output == nil {
@@ -92,7 +111,10 @@ func (c *httpClient) process(response *http.Response, output interface{}, expect
 	}
 
 	if err := dec.Decode(output); err != nil {
-		return errors.Wrap(err, "failed to load output")
+		return HTTPError{
+			err:  errors.Wrap(err, "failed to load output"),
+			resp: response,
+		}
 	}
 
 	return nil
