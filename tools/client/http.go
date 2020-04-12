dfb92a1433c82c56fe279348038fcb43b1617b5b
Muhamad Azamy <muhamad@incubaid.com>
2020-04-12 15:51:28 +0200

    remove debug line

--- tools/client/http.go
+++ tools/client/http.go
@@ -134,7 +134,6 @@ func (c *httpClient) post(u string, input interface{}, output interface{}, expec
 	if err := c.sign(req); err != nil {
 		return errors.Wrap(err, "failed to sign HTTP request")
 	}
-	fmt.Printf("%+v\n", req)
 	response, err := c.cl.Do(req)
 	if err != nil {
 		return errors.Wrap(err, "failed to send request")
