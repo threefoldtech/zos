909f5086e80c51630226be7680683a7af79ec937
Christophe de Carvalho <christophe.dcpm@gmail.com>
2020-04-22 18:05:30 +0200

    uncomment parked code

--- pkg/app/boot.go
+++ pkg/app/boot.go
@@ -5,24 +5,24 @@ import (
 	"path/filepath"
 )
 
-// BootedPath is the path where to store the booted flag
-var BootedPath = "/var/run/modules"
+// bootedPath is the path where to store the booted flag
+var bootedPath = "/var/run/modules"
 
 // MarkBooted creates a file in a memory
 // this file then can be used to check if "something" has been restared
 // if its the first time it starts
 func MarkBooted(name string) error {
-	if err := os.MkdirAll(BootedPath, 0770); err != nil {
+	if err := os.MkdirAll(bootedPath, 0770); err != nil {
 		return err
 	}
-	path := filepath.Join(BootedPath, name)
+	path := filepath.Join(bootedPath, name)
 	_, err := os.OpenFile(path, os.O_RDONLY|os.O_CREATE|os.O_TRUNC|os.O_EXCL, 0666)
 	return err
 }
 
 // IsFirstBoot checks if the a file has been created by MarkBooted function
 func IsFirstBoot(name string) bool {
-	path := filepath.Join(BootedPath, name)
+	path := filepath.Join(bootedPath, name)
 	_, err := os.Stat(path)
 	return !(err == nil)
 }
