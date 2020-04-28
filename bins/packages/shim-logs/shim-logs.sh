ebe7e6c2612e9a7aa0c2d51182fe61b71b5bc913
Maxime Daniel <root@maxux.net>
2020-04-28 22:50:49 +0200

    Add shim-logs extra bins package

---
+++ bins/packages/shim-logs/shim-logs.sh
@@ -0,0 +1,53 @@
+SHIMLOGS_VERSION="0.1"
+SHIMLOGS_CHECKSUM="0fbc114c8becf012cf9bb6c734dbc3d9"
+SHIMLOGS_LINK="https://github.com/threefoldtech/shim-logs/archive/v${SHIMLOGS_VERSION}.tar.gz"
+
+dependencies_shimlogs() {
+    apt-get install -y libjansson-dev libhiredis-dev
+}
+
+download_shimlogs() {
+    download_file ${SHIMLOGS_LINK} ${SHIMLOGS_CHECKSUM} shim-logs-${SHIMLOGS_VERSION}.tar.gz
+}
+
+extract_shimlogs() {
+    tar -xf ${DISTDIR}/shim-logs-${SHIMLOGS_VERSION}.tar.gz -C ${WORKDIR}
+}
+
+prepare_shimlogs() {
+    echo "[+] prepare shim-logs"
+    github_name "shim-logs-${SHIMLOGS_VERSION}"
+}
+
+compile_shimlogs() {
+    echo "[+] compile shim-logs"
+    make
+}
+
+install_shimlogs() {
+    echo "[+] install shim-logs"
+
+    mkdir -p "${ROOTDIR}/bin"
+
+    cp shim-logs ${ROOTDIR}/bin/shim-logs
+
+    chmod +x ${ROOTDIR}/bin/*
+}
+
+build_shimlogs() {
+    pushd "${DISTDIR}"
+
+    dependencies_shimlogs
+    download_shimlogs
+    extract_shimlogs
+
+    popd
+    pushd ${WORKDIR}/shim-logs-${SHIMLOGS_VERSION}
+
+    prepare_shimlogs
+    compile_shimlogs
+    install_shimlogs
+
+    popd
+}
+
