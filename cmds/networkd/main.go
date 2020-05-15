threefoldtech/zos

commit e1a8692dce83f76bbd30e85a504de54b561a9a86

adds reporting of ndmz pub4 and pub6 ifaces, #632 (#772)

cmds/networkd/main.go
@@ -68,14 +68,6 @@ func main() {
 		log.Info().Msg("shutting down")
 	})
 
-	ifaces, err := getLocalInterfaces()
-	if err != nil {
-		log.Fatal().Err(err).Msg("failed to read local network interfaces")
-	}
-	if err := publishIfaces(ifaces, nodeID, dir); err != nil {
-		log.Fatal().Err(err).Msg("failed to publish network interfaces to BCDB")
-	}
-
 	ifaceVersion := -1
 
 	exitIface, err := getPubIface(dir, nodeID.Identity())
@@ -122,6 +114,22 @@ func main() {
 			}
 		}
 	}(ctx, chIface)
+
+	ifaces, err := getLocalInterfaces()
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to read local network interfaces")
+	}
+
+	ndmzIfaces, err := getNdmzInterfaces()
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to read ndmz network interfaces")
+	}
+
+	ifaces = append(ifaces, ndmzIfaces...)
+
+	if err := publishIfaces(ifaces, nodeID, dir); err != nil {
+		log.Fatal().Err(err).Msg("failed to publish ndmz network interfaces to BCDB")
+	}
 
 	go startAddrWatch(ctx, nodeID, dir, ifaces)
 

cmds/networkd/addr_watcher.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/containernetworking/plugins/pkg/ns"
 	"github.com/rs/zerolog/log"
 
 	"time"
@@ -11,10 +12,12 @@ import (
 	"github.com/cenkalti/backoff/v3"
 	"github.com/threefoldtech/tfexplorer/client"
 	"github.com/threefoldtech/tfexplorer/models/generated/directory"
+	"github.com/threefoldtech/tfexplorer/schema"
 	"github.com/threefoldtech/zos/pkg"
 	"github.com/threefoldtech/zos/pkg/network/ifaceutil"
+	"github.com/threefoldtech/zos/pkg/network/namespace"
+	"github.com/threefoldtech/zos/pkg/network/ndmz"
 	"github.com/threefoldtech/zos/pkg/network/types"
-	"github.com/threefoldtech/tfexplorer/schema"
 	"github.com/vishvananda/netlink"
 )
 
@@ -56,6 +59,13 @@ func (w WatchedLinks) callBack(update netlink.AddrUpdate) error {
 		return err
 	}
 
+	ndmzIfaces, err := getNdmzInterfaces()
+	if err != nil {
+		return err
+	}
+
+	ifaces = append(ifaces, ndmzIfaces...)
+
 	return publishIfaces(ifaces, w.nodeID, w.dir)
 }
 
@@ -94,6 +104,53 @@ func (w WatchedLinks) Forever(ctx context.Context) error {
 			}
 		}
 	}
+}
+
+func getNdmzInterfaces() ([]types.IfaceInfo, error) {
+	var output []types.IfaceInfo
+
+	f := func(_ ns.NetNS) error {
+		links, err := netlink.LinkList()
+		if err != nil {
+			log.Error().Err(err).Msgf("failed to list interfaces")
+			return err
+		}
+		for _, link := range links {
+			if link.Attrs().Name == ndmz.DMZPub4 || link.Attrs().Name == ndmz.DMZPub6 {
+				addrs, err := netlink.AddrList(link, netlink.FAMILY_ALL)
+				if err != nil {
+					return err
+				}
+
+				info := types.IfaceInfo{
+					Name:       link.Attrs().Name,
+					Addrs:      make([]types.IPNet, len(addrs)),
+					MacAddress: schema.MacAddress{link.Attrs().HardwareAddr},
+				}
+				for i, addr := range addrs {
+					info.Addrs[i] = types.NewIPNet(addr.IPNet)
+				}
+
+				output = append(output, info)
+			}
+
+		}
+		return nil
+	}
+
+	// get the ndmz network namespace
+	ndmz, err := namespace.GetByName(ndmz.NetNSNDMZ)
+	if err != nil {
+		return nil, err
+	}
+	defer ndmz.Close()
+
+	err = ndmz.Do(f)
+	if err != nil {
+		return nil, err
+	}
+
+	return output, nil
 }
 
 func getLocalInterfaces() ([]types.IfaceInfo, error) {

commit ad3c6903f7be0b0c0db9cfb014005f70160aacfc

networkd: send ifaces information as soon as possible in the boot process
fix a bug introduced in https://github.com/threefoldtech/zos/commit/e1a8692dce83f76bbd30e85a504de54b561a9a86#diff-cb6a758eace8d4732038725e48d7beadL71
which delay the report of the network intefaces configuration after the
ndmz is created. This leads to no interfaces reported at all in the case
the ndmz failed to be created.

Since ifaces informartion are used by farmer to identity a node, having
no iface inforamtion at all was a problem.

networkd now tries to send iface info as soon as possible and another
time after ndmz is created.

cmds/networkd/main.go
@@ -68,6 +68,16 @@ func main() {
 		log.Info().Msg("shutting down")
 	})
 
+	// already sends all the interfaces detail we find
+	// this won't contains the ndmz IP yet, but this is OK.
+	ifaces, err := getLocalInterfaces()
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to read local network interfaces")
+	}
+	if err := publishIfaces(ifaces, nodeID, dir); err != nil {
+		log.Fatal().Err(err).Msg("failed to publish network interfaces to BCDB")
+	}
+
 	ifaceVersion := -1
 
 	exitIface, err := getPubIface(dir, nodeID.Identity())
@@ -115,16 +125,10 @@ func main() {
 		}
 	}(ctx, chIface)
 
-	ifaces, err := getLocalInterfaces()
-	if err != nil {
-		log.Fatal().Err(err).Msg("failed to read local network interfaces")
-	}
-
 	ndmzIfaces, err := getNdmzInterfaces()
 	if err != nil {
 		log.Fatal().Err(err).Msg("failed to read ndmz network interfaces")
 	}
-
 	ifaces = append(ifaces, ndmzIfaces...)
 
 	if err := publishIfaces(ifaces, nodeID, dir); err != nil {