eb52e443221a5e3086eb83619ebbc3186f53a8f3
dylanVerstraete <dylan_verstraete1@hotmail.com>
2020-06-30 14:42:50 +0200

    fix issue in build hd-idle script

--- bins/packages/hd-idle/hd-idle.sh
+++ bins/packages/hd-idle/hd-idle.sh
@@ -1,54 +1,54 @@
 HDIDLE_VERSION="master"
 HDIDLE_REPOSITORY="https://github.com/adelolmo/hd-idle.git"
 
-dependencies_hd_idle() {
+dependencies_hd-idle() {
     . ${PKGDIR}/../golang/golang.sh
     build_golang
 
     HD_IDLE_HOME="${GOPATH}/src/github.com/adelolmo"
 }
 
-download_hd_idle() {
+download_hd-idle() {
     download_git ${HDIDLE_REPOSITORY} ${HDIDLE_VERSION}
 }
 
-extract_hd_idle() {
+extract_hd-idle() {
     event "refreshing" "hd-idle-${HDIDLE_VERSION}"
     mkdir -p ${HD_IDLE_HOME}
     rm -rf ${HD_IDLE_HOME}/hd-idle
     cp -a ${DISTDIR}/hd-idle ${HD_IDLE_HOME}/
 }
 
-prepare_hd_idle() {
+prepare_hd-idle() {
     echo "[+] prepare hd-idle"
     github_name "hd-idle-${HDIDLE_VERSION}"
 }
 
-compile_hd_idle() {
+compile_hd-idle() {
     echo "[+] compiling hd-idle"
     dpkg-buildpackage -a armhf -us -uc -b
 }
 
-install_hd_idle() {
+install_hd-idle() {
     echo "[+] install hd-idle"
 
     mkdir -p "${ROOTDIR}/sbin"
     cp -av hd-idle "${ROOTDIR}/sbin/"
 }
 
-build_hd_idle() {
+build_hd-idle() {
     pushd "${DISTDIR}"
 
-    dependencies_hd_idle
-    download_hd_idle
-    extract_hd_idle
+    dependencies_hd-idle
+    download_hd-idle
+    extract_hd-idle
 
     popd
     pushd ${HD_IDLE_HOME}/hd-idle
 
-    prepare_hd_idle
-    compile_hd_idle
-    install_hd_idle
+    prepare_hd-idle
+    compile_hd-idle
+    install_hd-idle
 
     popd
 }
