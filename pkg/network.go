206f73858b464b5af2e8e5e4bc62a11ddca2109b
Muhamad Azamy <muhamad@incubaid.com>
2020-09-07 17:32:35 +0200

    Make sure to validate chached net-resource
    Fixes #918

--- pkg/network.go
+++ pkg/network.go
@@ -2,6 +2,7 @@ package pkg
 
 import (
 	"context"
+	"fmt"
 	"net"
 
 	"github.com/threefoldtech/zos/pkg/network/types"
@@ -117,6 +118,48 @@ type NetResource struct {
 	Peers []Peer `json:"peers"`
 }
 
+// Valid checks if the network resource is valid.
+func (nr *NetResource) Valid() error {
+	if nr.NetID == "" {
+		return fmt.Errorf("network ID cannot be empty")
+	}
+
+	if nr.Name == "" {
+		return fmt.Errorf("network name cannot be empty")
+	}
+
+	if nr.NetworkIPRange.Nil() {
+		return fmt.Errorf("network IP range cannot be empty")
+	}
+
+	if nr.NodeID == "" {
+		return fmt.Errorf("network resource node ID cannot empty")
+	}
+	if len(nr.Subnet.IP) == 0 {
+		return fmt.Errorf("network resource subnet cannot empty")
+	}
+
+	if nr.WGPrivateKey == "" {
+		return fmt.Errorf("network resource wireguard private key cannot empty")
+	}
+
+	if nr.WGPublicKey == "" {
+		return fmt.Errorf("network resource wireguard public key cannot empty")
+	}
+
+	if nr.WGListenPort == 0 {
+		return fmt.Errorf("network resource wireguard listen port cannot empty")
+	}
+
+	for _, peer := range nr.Peers {
+		if err := peer.Valid(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
 // Peer is the description of a peer of a NetResource
 type Peer struct {
 	// IPV4 subnet of the network resource of the peer
@@ -137,3 +180,19 @@ var (
 	// NetworkSchemaLatestVersion network object latest version
 	NetworkSchemaLatestVersion = NetworkSchemaV2
 )
+
+// Valid checks if peer is valid
+func (p *Peer) Valid() error {
+	if p.WGPublicKey == "" {
+		return fmt.Errorf("peer wireguard public key cannot empty")
+	}
+
+	if p.Subnet.Nil() {
+		return fmt.Errorf("peer wireguard subnet cannot empty")
+	}
+
+	if len(p.AllowedIPs) <= 0 {
+		return fmt.Errorf("peer wireguard allowedIPs cannot empty")
+	}
+	return nil
+}
