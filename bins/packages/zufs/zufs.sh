9b13f54232e90f7b22c14eb849719d58c3007c52
Muhamad Azmy <muhamada@greenitglobe.com>
2020-09-24 12:40:11 +0200

    release 0-fs 2.0.6 (#971)

--- bins/packages/zufs/zufs.sh
+++ bins/packages/zufs/zufs.sh
@@ -1,5 +1,5 @@
-ZUFS_VERSION="2.0.5"
-ZUFS_CHECKSUM="39b5ac929253a9c92147c2a6862cc68c"
+ZUFS_VERSION="2.0.6"
+ZUFS_CHECKSUM="06368fd114642373c1bb024bad2d419e"
 ZUFS_LINK="https://github.com/threefoldtech/0-fs/archive/v${ZUFS_VERSION}.tar.gz"
 
 dependencies_zufs() {
