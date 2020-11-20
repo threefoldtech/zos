3715adb23182c95a858407a3c713012a8375a291
Lee Smet <lee.smet@hotmail.com>
2020-11-20 18:18:13 +0100

    Temporarily use a different k3os package
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

--- pkg/provision/primitives/kubernetes.go
+++ pkg/provision/primitives/kubernetes.go
@@ -52,7 +52,8 @@ type Kubernetes struct {
 	PlainClusterSecret string `json:"-"`
 }
 
-const k3osFlistURL = "https://hub.grid.tf/tf-official-apps/k3os.flist"
+// const k3osFlistURL = "https://hub.grid.tf/tf-official-apps/k3os.flist"
+const k3osFlistURL = "https://hub.grid.tf/lee/k3os.flist"
 
 func (p *Provisioner) kubernetesProvision(ctx context.Context, reservation *provision.Reservation) (interface{}, error) {
 	return p.kubernetesProvisionImpl(ctx, reservation)
