1cbf6442e9fc2e41228093ab0dfd6ef5da48fd0b
Lee Smet <lee.smet@hotmail.com>
2020-11-23 12:43:22 +0100

    Properly derive the ipv6 private gateway
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

--- pkg/network/networker.go
+++ pkg/network/networker.go
@@ -522,7 +522,8 @@ func (n networker) GetDefaultGwIP(networkID pkg.NetID) (net.IP, net.IP, error) {
 	// also a subnet in a NR is assumed to be a /24
 	ip[len(ip)-1] = 1
 
-	return ip, net.ParseIP("fe80::1"), nil
+	// ipv6 is derived from the ipv4
+	return ip, nr.Convert4to6(string(networkID), ip), nil
 }
 
 // GetIPv6From4 generates an IPv6 address from a given IPv4 address in a NR
