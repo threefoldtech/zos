b4908dc5812b1693edc8cb9cec62fbd439a51ff8
Muhamad Azmy <muhamad@incubaid.com>
2021-03-02 11:39:08 +0100

    clean up yggdrasil for resuability by the gateway (#1204)

--- pkg/network/yggdrasil/public_peerlist.go
+++ pkg/network/yggdrasil/public_peerlist.go
@@ -7,22 +7,18 @@ import (
 
 //PeerListFallback is an hardcoded list of public yggdrasil node
 // it is used to have some available peer to connect to when we failed to read the online public peer info
-var PeerListFallback = PeerList{
-	peers: map[string]map[string]NodeInfo{
-		"fallback.md": {
-			"tls://45.147.198.155:6010": {
-				Endpoint: "tls://45.147.198.155:6010",
-				Up:       true,
-			},
-			"tcp://85.17.15.221:35239": {
-				Endpoint: "tcp://85.17.15.221:35239",
-				Up:       true,
-			},
-			"tcp://51.255.223.60:64982": {
-				Endpoint: "tcp://51.255.223.60:64982",
-				Up:       true,
-			},
-		},
+var PeerListFallback = Peers{
+	{
+		Endpoint: "tls://45.147.198.155:6010",
+		Up:       true,
+	},
+	{
+		Endpoint: "tcp://85.17.15.221:35239",
+		Up:       true,
+	},
+	{
+		Endpoint: "tcp://51.255.223.60:64982",
+		Up:       true,
 	},
 }
 
@@ -35,56 +31,42 @@ type NodeInfo struct {
 	LastSeen  int
 }
 
-// PeerList is a list of yggsdrasil peer retrieved from https://publicpeers.neilalexander.dev/publicnodes.json
-type PeerList struct {
-	peers map[string]map[string]NodeInfo
-}
+// Peers is a peers list
+type Peers []NodeInfo
 
 // FetchPeerList download the list of public yggdrasil peer from https://publicpeers.neilalexander.dev/publicnodes.json
-func FetchPeerList() (PeerList, error) {
-	pl := PeerList{}
+func FetchPeerList() (Peers, error) {
+	//pl := PeerList{}
+	pl := map[string]map[string]NodeInfo{}
 
 	resp, err := http.Get("https://publicpeers.neilalexander.dev/publicnodes.json")
 	if err != nil {
-		return pl, err
+		return nil, err
 	}
 
-	if err := json.NewDecoder(resp.Body).Decode(&pl.peers); err != nil {
-		return pl, err
+	if err := json.NewDecoder(resp.Body).Decode(&pl); err != nil {
+		return nil, err
 	}
 
-	for country := range pl.peers {
-		for endpoint := range pl.peers[country] {
-			info := pl.peers[country][endpoint]
-			info.Endpoint = endpoint
-			pl.peers[country][endpoint] = info
+	var peers Peers
+	for _, nodes := range pl {
+		for endpoint, node := range nodes {
+			node.Endpoint = endpoint
+			peers = append(peers, node)
 		}
 	}
 
-	return pl, nil
-}
-
-// Peers return all the peers information from the PeerList p
-func (p PeerList) Peers() []NodeInfo {
-	peers := make([]NodeInfo, 0, len(p.peers)*2)
-	for _, l := range p.peers {
-		for _, info := range l {
-			peers = append(peers, info)
-		}
-	}
-	return peers
+	return peers, nil
 }
 
 // Ups return all the peers that are marked up from the PeerList p
-func (p PeerList) Ups() []NodeInfo {
-	a := p.Peers()
-	n := 0
-	for _, x := range a {
-		if x.Up {
-			a[n] = x
-			n++
+func (p Peers) Ups() Peers {
+	var peers Peers
+	for _, n := range p {
+		if n.Up {
+			peers = append(peers, n)
 		}
 	}
-	a = a[:n]
-	return a
+
+	return peers
 }
--- pkg/network/yygdrasil_test.go
+++ pkg/network/yygdrasil_test.go
@@ -1,4 +1,4 @@
-package yggdrasil
+package network
 
 import (
 	"net"
@@ -6,6 +6,7 @@ import (
 
 	"github.com/stretchr/testify/require"
 	"github.com/threefoldtech/zos/pkg/identity"
+	"github.com/threefoldtech/zos/pkg/network/yggdrasil"
 	"gotest.tools/assert"
 )
 
@@ -13,21 +14,21 @@ func TestGenerateConfig(t *testing.T) {
 	kp, err := identity.FromSeed([]byte("00000000000000000000000000000000"))
 	require.NoError(t, err)
 
-	cfg := GenerateConfig(kp.PrivateKey)
+	cfg := yggdrasil.GenerateConfig(kp.PrivateKey)
 
 	assert.Equal(t, "30303030303030303030303030303030303030303030303030303030303030301ba4075b77c9e3fb3ecde15cdaf5221f3c10373e623f7b0e1ef76366b0af7137", cfg.SigningPrivateKey)
 	assert.Equal(t, "1ba4075b77c9e3fb3ecde15cdaf5221f3c10373e623f7b0e1ef76366b0af7137", cfg.SigningPublicKey)
 	assert.Equal(t, "98b6d128682e280b74b324ca82a6bae6e8a3f7174e0605bfd52eb9948fad8944", cfg.EncryptionPrivateKey)
 	assert.Equal(t, "167315f5a03796214692d3a74b0a48d630f7fa5e5257730da0415dc7af7ab260", cfg.EncryptionPublicKey)
-	assert.Equal(t, "2ru5PcgeQzxF7QZYwQgDkG2K13PRqyigVw99zMYg8eML", cfg.NodeInfo["name"])
+	assert.Equal(t, "2ru5Pc", cfg.NodeInfo["name"])
 }
 
 func TestAddresses(t *testing.T) {
 	kp, err := identity.FromSeed([]byte("00000000000000000000000000000000"))
 	require.NoError(t, err)
 
-	cfg := GenerateConfig(kp.PrivateKey)
-	s := NewServer(nil, &cfg)
+	cfg := yggdrasil.GenerateConfig(kp.PrivateKey)
+	s := NewYggServer(nil, &cfg)
 
 	ip, err := s.Address()
 	require.NoError(t, err)
