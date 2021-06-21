4df1226e3c0a147069bfc84983f89ee9b10de838
Muhamad Azamy <muhamad@incubaid.com>
2021-06-21 12:41:20 +0200

    add more methods to client

--- client/node.go
+++ client/node.go
@@ -3,6 +3,7 @@ package client
 import (
 	"context"
 
+	"github.com/threefoldtech/zos/pkg"
 	"github.com/threefoldtech/zos/pkg/gridtypes"
 	"github.com/threefoldtech/zos/pkg/rmb"
 )
@@ -18,17 +19,17 @@ func NewNodeClient(nodeTwin uint32, bus rmb.Client) *NodeClient {
 	return &NodeClient{nodeTwin, bus}
 }
 
-func (n *NodeClient) Deploy(ctx context.Context, dl gridtypes.Deployment) error {
+func (n *NodeClient) DeploymentDeploy(ctx context.Context, dl gridtypes.Deployment) error {
 	const cmd = "zos.deployment.deploy"
 	return n.bus.Call(ctx, n.nodeTwin, cmd, dl, nil)
 }
 
-func (n *NodeClient) Update(ctx context.Context, dl gridtypes.Deployment) error {
+func (n *NodeClient) DeploymentUpdate(ctx context.Context, dl gridtypes.Deployment) error {
 	const cmd = "zos.deployment.update"
 	return n.bus.Call(ctx, n.nodeTwin, cmd, dl, nil)
 }
 
-func (n *NodeClient) Get(ctx context.Context, deploymentID uint32) (dl gridtypes.Deployment, err error) {
+func (n *NodeClient) DeploymentGet(ctx context.Context, deploymentID uint32) (dl gridtypes.Deployment, err error) {
 	const cmd = "zos.deployment.get"
 	in := args{
 		"deployment_id": deploymentID,
@@ -41,7 +42,7 @@ func (n *NodeClient) Get(ctx context.Context, deploymentID uint32) (dl gridtypes
 	return dl, nil
 }
 
-func (n *NodeClient) Delete(ctx context.Context, deploymentID uint32) error {
+func (n *NodeClient) DeploymentDelete(ctx context.Context, deploymentID uint32) error {
 	const cmd = "zos.deployment.delete"
 	in := args{
 		"deployment_id": deploymentID,
@@ -61,4 +62,36 @@ func (n *NodeClient) Counters(ctx context.Context) (total gridtypes.Capacity, us
 	}
 
 	return result.Total, result.Used, nil
+}
+
+func (n *NodeClient) NetworkListWGPorts(ctx context.Context) ([]uint16, error) {
+	const cmd = "zos.network.list_wg_ports"
+	var result []uint16
+
+	if err := n.bus.Call(ctx, n.nodeTwin, cmd, nil, &result); err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func (n *NodeClient) NetworkListIPs(ctx context.Context) ([]string, error) {
+	const cmd = "zos.network.list_public_ips"
+	var result []string
+
+	if err := n.bus.Call(ctx, n.nodeTwin, cmd, nil, &result); err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func (n *NodeClient) NetworkGetPublicConfig(ctx context.Context) (cfg pkg.PublicConfig, err error) {
+	const cmd = "zos.network.public_config_get"
+
+	if err = n.bus.Call(ctx, n.nodeTwin, cmd, nil, &cfg); err != nil {
+		return
+	}
+
+	return
 }
