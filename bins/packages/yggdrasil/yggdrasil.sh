7a22e7e4a57dcbf5c3294824e19f58875216fc99
Muhamad Azamy <muhamad@incubaid.com>
2021-08-02 16:12:27 +0200

    fix version

--- bins/packages/yggdrasil/yggdrasil.sh
+++ bins/packages/yggdrasil/yggdrasil.sh
@@ -1,4 +1,4 @@
-YGGDRASIL_VERSION="v0.4.0"
+YGGDRASIL_VERSION="0.4.0"
 YGGDRASIL_CHECKSUM="5f5d58c904392bd892816f5ba793c85f"
 YGGDRASIL_LINK="https://github.com/yggdrasil-network/yggdrasil-go/releases/download/v${YGGDRASIL_VERSION}/yggdrasil-${YGGDRASIL_VERSION}-amd64.deb"
 
