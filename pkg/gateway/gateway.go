88a7faf74e6c461129e25f3db9312a51e20d3611
OmarElawady <omarelawady1998@gmail.com>
2021-08-31 18:35:25 +0200

    wip: implement gateway nameproxy

--- pkg/gateway/gateway.go
+++ pkg/gateway/gateway.go
@@ -1,7 +1,166 @@
 package gateway
 
-import "github.com/threefoldtech/zos/pkg"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog/log"
+	"github.com/threefoldtech/zos/pkg"
+	"github.com/threefoldtech/zos/pkg/network/namespace"
+	"github.com/threefoldtech/zos/pkg/network/types"
+	"github.com/threefoldtech/zos/pkg/zinit"
+	"gopkg.in/yaml.v2"
+)
+
+const (
+	traefikService = "traefik"
+)
+
+type gatewayModule struct {
+	proxyConfigPath string
+	traefikStarted  bool
+}
+
+type ProxyConfig struct {
+	Http HTTPConfig
+}
+
+type HTTPConfig struct {
+	Routers  map[string]Router
+	Services map[string]Service
+}
+
+type Router struct {
+	Rule    string
+	Service string
+}
+
+type Service struct {
+	LoadBalancer LoadBalancer
+}
+
+type LoadBalancer struct {
+	Servers []Server
+}
+
+type Server struct {
+	Url string
+}
 
 func New(root string) pkg.Gateway {
-	panic("not implemented")
+	configPath := filepath.Join(root, "proxy")
+	// where should service-restart/node-reboot recovery be handled?
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		err := os.MkdirAll(configPath, 0644)
+		if err != nil {
+			// ok to panic?
+			panic(errors.Wrap(err, "couldn't make gateway config dir"))
+		}
+	}
+
+	g := &gatewayModule{
+		proxyConfigPath: configPath,
+		traefikStarted:  false,
+	}
+	log.Debug().Bool("exist", namespace.Exists(types.PublicNamespace)).Msg("checking public namespace")
+	if !isTraefikStarted() && namespace.Exists(types.PublicNamespace) {
+		err := g.startTraefik()
+		if err != nil {
+			log.Error().Err(err).Msg("couldn't start traefik")
+		}
+	}
+	return g
+}
+func isTraefikStarted() bool {
+	z, err := zinit.New("")
+	if err != nil {
+		panic(errors.Wrap(err, "couldn't get zinit client"))
+	}
+	defer z.Close()
+	started := true
+	traefikStatus, err := z.Status(traefikService)
+	if err != nil {
+		started = false
+	}
+	log.Debug().Str("state", traefikStatus.State.String()).Msg("checking traefik state")
+	started = traefikStatus.State.String() == zinit.ServiceStateRunning
+	return started
+}
+func (g *gatewayModule) startTraefik() error {
+	z, err := zinit.New("")
+	if err != nil {
+		return errors.Wrap(err, "couldn't get zinit client")
+	}
+	defer z.Close()
+	cmd := fmt.Sprintf("ip netns exec public traefik --log.level=DEBUG --providers.file.directory=%s --providers.file.watch=true", g.proxyConfigPath)
+	zinit.AddService(traefikService, zinit.InitService{
+		Exec: cmd,
+	})
+	if err := z.Monitor(traefikService); err != nil {
+		return errors.Wrap(err, "couldn't monitor traefik service")
+	}
+	if err := z.StartWait(time.Second*20, traefikService); err != nil {
+		return errors.Wrap(err, "waiting for trafik start timed out")
+	}
+	g.traefikStarted = true
+	return nil
+}
+
+func (g *gatewayModule) SetNamedProxy(fqdn string, backends []string) error {
+	if !g.traefikStarted {
+		if namespace.Exists(types.PublicNamespace) {
+			if err := g.startTraefik(); err != nil {
+				return errors.Wrap(err, "couldn't start traefik")
+			}
+		} else {
+			return errors.New("node doesn't support gateway workloads as it doesn't have public config")
+		}
+	}
+
+	rule := fmt.Sprintf("Host(`%s`) && PathPrefix(`/`)", fqdn)
+	servers := make([]Server, len(backends))
+	for idx, backend := range backends {
+		servers[idx] = Server{
+			Url: backend,
+		}
+	}
+	config := ProxyConfig{
+		Http: HTTPConfig{
+			Routers: map[string]Router{
+				fqdn: {
+					Rule:    rule,
+					Service: fqdn,
+				},
+			},
+			Services: map[string]Service{
+				fqdn: {
+					LoadBalancer: LoadBalancer{
+						Servers: servers,
+					},
+				},
+			},
+		},
+	}
+
+	yamlString, err := yaml.Marshal(&config)
+	if err != nil {
+		return errors.Wrap(err, "failed to convert config to yaml")
+	}
+	log.Debug().Str("yaml_config", string(yamlString)).Msg("configuration file")
+	filename := filepath.Join(g.proxyConfigPath, fmt.Sprintf("%s.yaml", fqdn))
+	if err = os.WriteFile(filename, yamlString, 0644); err != nil {
+		return errors.Wrap(err, "couldn't open config file for writing")
+	}
+
+	return nil
+}
+func (g *gatewayModule) DeleteNamedProxy(fqdn string) error {
+	filename := filepath.Join(g.proxyConfigPath, fmt.Sprintf("%s.yaml", fqdn))
+	if err := os.Remove(filename); err != nil {
+		return errors.Wrap(err, "couldn't remove config file")
+	}
+	return nil
 }
--- pkg/primitives/gateway.go
+++ pkg/primitives/gateway.go
@@ -2,16 +2,49 @@ package primitives
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 
+	"github.com/pkg/errors"
 	"github.com/threefoldtech/zos/pkg/gridtypes"
+	"github.com/threefoldtech/zos/pkg/gridtypes/zos"
+	"github.com/threefoldtech/zos/pkg/provision"
+	"github.com/threefoldtech/zos/pkg/stubs"
 )
 
+func validateNameContract(twinID uint32, name string) error {
+	// TODO: validate against substrate?
+	return nil
+}
+
+func getNodeDomain() (string, error) {
+	// TODO: how to get
+	return "omar.com", nil
+}
+
 func (p *Primitives) gwProvision(ctx context.Context, wl *gridtypes.WorkloadWithID) (interface{}, error) {
+
+	var proxy zos.GatewayNameProxy
+	if err := json.Unmarshal(wl.Data, &proxy); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal gateway proxy from reservation: %w", err)
+	}
+	baseDomain, err := getNodeDomain()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get domain base domain")
+	}
+	fqdn := fmt.Sprintf("%s.%s", proxy.Name, baseDomain)
+	backends := make([]string, len(proxy.Backends))
+	for idx, backend := range proxy.Backends {
+		backends[idx] = string(backend)
+	}
 	// what we need to do:
 	// - does this node support gateways ?
 	// this can be validated by checking if we have a "public" namespace
-
+	deployment := provision.GetDeployment(ctx)
+	twinID := deployment.TwinID
+	if err := validateNameContract(twinID, proxy.Name); err != nil {
+		return nil, errors.Wrap(err, "failed to validate name contract")
+	}
 	// - Validation of ownership of the name (later)
 	// this must be done against substrate. Make sure that same user (twin) owns the
 	// name int he workload config
@@ -19,10 +52,33 @@ func (p *Primitives) gwProvision(ctx context.Context, wl *gridtypes.WorkloadWith
 	// - make necessary calls to gateway daemon.
 	// gateway := stubs.NewGatewayStub(p.zbus)
 	// gateway.SetNamedProxy(ctx context.Context, arg0 string, arg1 []string)
-	return nil, fmt.Errorf("not implemented")
+	gateway := stubs.NewGatewayStub(p.zbus)
+	if err := gateway.SetNamedProxy(ctx, fqdn, backends); err != nil {
+		return nil, errors.Wrap(err, "failed to setup name proxy")
+	}
+	return nil, nil
 }
 
 func (p *Primitives) wgDecommission(ctx context.Context, wl *gridtypes.WorkloadWithID) error {
-	// - make necessary calls to gateway daemon
-	return fmt.Errorf("not implemented")
+	deployment := provision.GetDeployment(ctx)
+	twinID := deployment.TwinID
+	var proxy zos.GatewayNameProxy
+	if err := json.Unmarshal(wl.Data, &proxy); err != nil {
+		return fmt.Errorf("failed to unmarshal gateway proxy from reservation: %w", err)
+	}
+	if err := validateNameContract(twinID, proxy.Name); err != nil {
+		return errors.Wrap(err, "failed to validate name contract")
+	}
+
+	baseDomain, err := getNodeDomain()
+	if err != nil {
+		return errors.Wrap(err, "failed to get domain base domain")
+	}
+	fqdn := fmt.Sprintf("%s.%s", proxy.Name, baseDomain)
+
+	gateway := stubs.NewGatewayStub(p.zbus)
+	if err := gateway.DeleteNamedProxy(ctx, fqdn); err != nil {
+		return errors.Wrap(err, "failed to delete name proxy")
+	}
+	return nil
 }
