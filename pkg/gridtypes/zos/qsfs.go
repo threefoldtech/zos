29279eb91e945c8f237aac947ddc3ccd00d79be0
Muhamad Azamy <muhamad@incubaid.com>
2021-09-16 15:14:56 +0200

    wip: add basic types for qsfs

---
+++ pkg/gridtypes/zos/qsfs.go
@@ -0,0 +1,173 @@
+package zos
+
+import (
+	"crypto/aes"
+	"encoding/hex"
+	"fmt"
+	"io"
+
+	"github.com/threefoldtech/zos/pkg/gridtypes"
+)
+
+type EncryptionAlgorithm string
+type EncryptionKey []byte
+
+func (k EncryptionKey) Valid() error {
+	if len(k) != 32 {
+		return aes.KeySizeError(len(k))
+	}
+
+	return nil
+}
+
+func (k EncryptionKey) MarshalText() ([]byte, error) {
+	return []byte(hex.EncodeToString(k)), nil
+}
+
+func (k *EncryptionKey) UnmarshalText(data []byte) error {
+	b, err := hex.DecodeString(string(data))
+	if err != nil {
+		return err
+	}
+	*k = b
+	return nil
+}
+
+type Encryption struct {
+	Algorithm EncryptionAlgorithm `json:"algorithm" toml:"algorithm"`
+	Key       EncryptionKey       `json:"key" toml:"key"`
+}
+
+func (c *Encryption) Challenge(w io.Writer) error {
+	if _, err := fmt.Fprintf(w, "%s", c.Algorithm); err != nil {
+		return err
+	}
+	if _, err := fmt.Fprintf(w, "%x", c.Key); err != nil {
+		return err
+	}
+	return nil
+}
+
+type ZdbBackend struct {
+	Address   string `json:"address" toml:"address"`
+	Namespace string `json:"namespace" toml:"namespace"`
+	Password  string `json:"password" toml:"password"`
+}
+
+func (z *ZdbBackend) Challenge(w io.Writer) error {
+	if _, err := fmt.Fprintf(w, "%s", z.Address); err != nil {
+		return err
+	}
+
+	if _, err := fmt.Fprintf(w, "%s", z.Namespace); err != nil {
+		return err
+	}
+
+	if _, err := fmt.Fprintf(w, "%s", z.Password); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type QuantumSafeMeta struct {
+	Prefix     string       `json:"prefix" toml:"prefix"`
+	Encryption Encryption   `json:"encryption" toml:"encryption"`
+	Backends   []ZdbBackend `json:"backends" toml:"backends"`
+}
+
+func (m *QuantumSafeMeta) Challenge(w io.Writer) error {
+	if _, err := fmt.Fprintf(w, "%s", m.Prefix); err != nil {
+		return err
+	}
+
+	if err := m.Encryption.Challenge(w); err != nil {
+		return err
+	}
+
+	for _, be := range m.Backends {
+		if err := be.Challenge(w); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+type ZdbGroup struct {
+	Backends []ZdbBackend `json:"backends" toml:"backends"`
+}
+
+func (z *ZdbGroup) Challenge(w io.Writer) error {
+	for _, be := range z.Backends {
+		if err := be.Challenge(w); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+type QuantumSafeFSConfig struct {
+	DataShards      uint32          `json:"jsondata_shards" toml:"jsondata_shards"`
+	ParityShards    uint32          `json:"parity_shards" toml:"parity_shards"`
+	RedundantGroups uint32          `json:"redundant_groups" toml:"redundant_groups"`
+	RedundantNodes  uint32          `json:"redundant_nodes" toml:"redundant_nodes"`
+	Encryption      Encryption      `json:"encryption" toml:"encryption"`
+	Meta            QuantumSafeMeta `json:"meta" toml:"meta"`
+	Groups          []ZdbGroup      `json:"groups" toml:"groups"`
+}
+
+func (c *QuantumSafeFSConfig) Challenge(w io.Writer) error {
+	if _, err := fmt.Fprintf(w, "%d", c.DataShards); err != nil {
+		return err
+	}
+
+	if _, err := fmt.Fprintf(w, "%d", c.ParityShards); err != nil {
+		return err
+	}
+
+	if _, err := fmt.Fprintf(w, "%d", c.RedundantGroups); err != nil {
+		return err
+	}
+
+	if _, err := fmt.Fprintf(w, "%d", c.RedundantNodes); err != nil {
+		return err
+	}
+
+	if err := c.Encryption.Challenge(w); err != nil {
+		return err
+	}
+
+	if err := c.Meta.Challenge(w); err != nil {
+		return err
+	}
+
+	for _, g := range c.Groups {
+		if err := g.Challenge(w); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+type QuatumSafeFS struct {
+	Cache  gridtypes.Unit      `json:"cache"`
+	Config QuantumSafeFSConfig `json:"config"`
+}
+
+func (q QuatumSafeFS) Valid(getter gridtypes.WorkloadGetter) error {
+	return nil
+}
+
+func (q QuatumSafeFS) Challenge(w io.Writer) error {
+	if _, err := fmt.Fprintf(w, "%d", q.Cache); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (q QuatumSafeFS) Capacity() (gridtypes.Capacity, error) {
+	return gridtypes.Capacity{}, nil
+}
