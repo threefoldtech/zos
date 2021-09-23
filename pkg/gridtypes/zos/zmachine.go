c13d2ed51b3fb5dc54fce9d9db184cdb436d77fb
OmarElawady <omarelawady1998@gmail.com>
2021-09-23 11:37:22 +0200

    add ygg to vm result

--- pkg/gridtypes/zos/zmachine.go
+++ pkg/gridtypes/zos/zmachine.go
@@ -230,6 +230,7 @@ func (v ZMachine) Challenge(b io.Writer) error {
 
 // ZMachineResult result returned by VM reservation
 type ZMachineResult struct {
-	ID string `json:"id"`
-	IP string `json:"ip"`
+	ID    string `json:"id"`
+	IP    string `json:"ip"`
+	YggIP string `json:"ygg_ip"`
 }
