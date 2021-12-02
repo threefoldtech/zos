bfadc50495ce1dccf405ce77adb5f4f33ac8c337
Muhamad Azamy <muhamad@incubaid.com>
2021-12-02 16:15:57 +0100

    add workers number

--- cmds/modules/noded/twin.go
+++ cmds/modules/noded/twin.go
@@ -11,7 +11,7 @@ import (
 func runMsgBus(ctx context.Context, twin uint32, substrate string) error {
 	// todo: make it argument or parse from broker
 	const redis = "/var/run/redis.sock"
-	app, err := rmb.NewServer(substrate, redis, int(twin))
+	app, err := rmb.NewServer(substrate, redis, int(twin), 1000)
 	if err != nil {
 		return err
 	}
