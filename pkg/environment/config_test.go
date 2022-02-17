3cc11c0ab14c39ff9e484d12ad31f45985e2b84a
Muhamad Azamy <muhamad@incubaid.com>
2022-02-17 15:08:58 +0100

    add another test

--- pkg/environment/config_test.go
+++ pkg/environment/config_test.go
@@ -20,3 +20,16 @@ func TestConfigMerge(t *testing.T) {
 	require.Equal(t, []string{"a", "b", "c"}, cfg.Yggdrasil.Peers)
 	require.Len(t, cfg.Monitor, 3)
 }
+
+func TestConfigMergeEmpty(t *testing.T) {
+	var cfg Config
+	cfg.Monitor = []uint32{0, 1}
+	cfg.Yggdrasil.Peers = []string{"a", "b"}
+
+	var ex Config
+
+	cfg.Merge(ex)
+
+	require.Equal(t, []string{"a", "b"}, cfg.Yggdrasil.Peers)
+	require.Len(t, cfg.Monitor, 2)
+}
