3bf368c330b0b6ba531f85b5249760ba4c731308
Muhamad Azamy <muhamad@incubaid.com>
2022-04-26 10:02:36 +0200

    fix checksum

--- bins/packages/tailstream/tailstream.sh
+++ bins/packages/tailstream/tailstream.sh
@@ -1,5 +1,5 @@
 TAILSTREAM_VERSION="0.1.4"
-TAILSTREAM_CHECKSUM="e83ceadbbc3f41248199ffdb65e0de08"
+TAILSTREAM_CHECKSUM="f95edc9830f1b65606722b7f74bdd8d9"
 TAILSTREAM_LINK="https://github.com/threefoldtech/tailstream/releases/download/v${TAILSTREAM_VERSION}/tailstream"
 
 download_tailstream() {
