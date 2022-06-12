f51e712eeb8acf93639d7354faebc99728201517
Abdelrahman Ghanem <abom.jdev@gmail.com>
2022-06-12 13:10:19 +0200

    use os.exec insted of zinit

--- cmds/modules/noded/twin.go
+++ cmds/modules/noded/twin.go
@@ -7,13 +7,12 @@ import (
 	"fmt"
 	"github.com/pkg/errors"
 	"github.com/rs/zerolog/log"
-	"github.com/threefoldtech/zos/pkg/zinit"
-	"time"
+	"os/exec"
 )
 
 const (
-	busService = "rmb"
-	KeyType    = "ed25519"
+	redisAddr = "unix:///var/run/redis.sock"
+	keyType   = "ed25519"
 )
 
 func runMsgBus(ctx context.Context, sk ed25519.PrivateKey, substrateURLs []string) error {
@@ -24,23 +23,8 @@ func runMsgBus(ctx context.Context, sk ed25519.PrivateKey, substrateURLs []strin
 
 	seed := sk.Seed()
 	seedHex := fmt.Sprintf("0x%s", hex.EncodeToString(seed))
-	cmd := fmt.Sprintf(`/bin/rmb --substrate "%s" --key-type "%s" --seed "%s"`, substrateURLs[0], KeyType, seedHex)
 
-	// just for debugging for now
-	log.Info().Str("cmd", cmd).Msg("starting rmb with")
-
-	cl := zinit.Default()
-	err := zinit.AddService(busService, zinit.InitService{
-		Exec: cmd,
-	})
-
-	if err != nil {
-		return err
-	}
-
-	if err = cl.Monitor(busService); err != nil && !errors.Is(err, zinit.ErrAlreadyMonitored) {
-		return err
-	}
-
-	return cl.StartWait(time.Second*20, busService)
+	log.Info().Msg("starting rmb...")
+	command := exec.CommandContext(ctx, "rmb", "-s", substrateURLs[0], "-k", keyType, "--seed", seedHex, "-r", redisAddr)
+	return command.Run()
 }
