80b614eb91caa6488aee1a26066da03a9d186a87
Muhamad Azmy <muhamad@incubaid.com>
2022-06-17 20:36:34 +0200

    Update rfs to latest 0.2.7 (#1749)

--- bins/packages/zufs/zufs.sh
+++ bins/packages/zufs/zufs.sh
@@ -1,11 +1,10 @@
-ZUFS_VERSION="0.2.6"
-ZUFS_CHECKSUM="8d576939d75b12613abdfc1bd18204f6"
+ZUFS_VERSION="0.2.7"
+ZUFS_CHECKSUM="5a08c6a12aefbd139d39f1ef9194ad5b"
 ZUFS_LINK="https://github.com/threefoldtech/rfs/releases/download/v${ZUFS_VERSION}/rfs"
 
 download_zufs() {
     download_file ${ZUFS_LINK} ${ZUFS_CHECKSUM} rfs-${ZUFS_VERSION}
 }
-
 
 prepare_zufs() {
     echo "[+] prepare 0-fs"
