4a9bbc99a3c752d4422991ec15ec4d791441c7de
Muhamad Azamy <muhamad@incubaid.com>
2022-10-13 15:11:23 +0200

    Prefetch the node identity
    Prefetch the node identity and use it for uptime updates

--- cmds/modules/noded/register.go
+++ cmds/modules/noded/register.go
@@ -2,7 +2,6 @@ package noded
 
 import (
 	"context"
-	"crypto/ed25519"
 	"time"
 
 	"github.com/centrifuge/go-substrate-rpc-client/v4/types"
@@ -10,29 +9,15 @@ import (
 	"github.com/rs/zerolog/log"
 	"github.com/shirou/gopsutil/host"
 	"github.com/threefoldtech/substrate-client"
-	"github.com/threefoldtech/zbus"
 	"github.com/threefoldtech/zos/pkg/environment"
-	"github.com/threefoldtech/zos/pkg/stubs"
 )
 
 const (
 	reportUptimeEvery = 40 * time.Minute
 )
 
-func uptime(ctx context.Context, cl zbus.Client) error {
-	var (
-		mgr = stubs.NewIdentityManagerStub(cl)
-	)
-
+func uptime(ctx context.Context, id substrate.Identity) error {
 	subMgr, err := environment.GetSubstrate()
-	if err != nil {
-		return err
-	}
-
-	busCtx, cancel := context.WithTimeout(ctx, 10*time.Second)
-	defer cancel()
-	sk := ed25519.PrivateKey(mgr.PrivateKey(busCtx))
-	id, err := substrate.NewIdentityFromEd25519Key(sk)
 	if err != nil {
 		return err
 	}
