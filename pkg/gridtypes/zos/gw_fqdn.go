73aefb61e95787623837cc0ae81092c99594b5ea
rawdaGastan <rody.rawda.rr@gmail.com>
2023-02-21 15:33:50 +0200

    validate backend

--- pkg/gridtypes/zos/gw_fqdn.go
+++ pkg/gridtypes/zos/gw_fqdn.go
@@ -6,6 +6,7 @@ import (
 	"net"
 	"net/url"
 	"regexp"
+	"strconv"
 
 	"github.com/pkg/errors"
 	"github.com/threefoldtech/zos/pkg/gridtypes"
@@ -19,24 +20,41 @@ type Backend string
 
 // check if valid http://ip:port, http://ip or ip:port
 func (b Backend) Valid(tls bool) error {
-	u, err := url.Parse(string(b))
-	if err != nil {
-		return errors.Wrap(err, "failed to parse backend")
-	}
+	var hostName string
 	if tls {
-		if u.Scheme != "" {
-			return fmt.Errorf("scheme expected to be empty")
+		host, port, err := net.SplitHostPort(string(b))
+		if err != nil {
+			return fmt.Errorf("failed to parse backend with error: %w", err)
 		}
-		if u.Port() == "" {
-			return fmt.Errorf("missing port in backend address")
+		if port == "" {
+			return fmt.Errorf("missing port in backend")
 		}
-	} else if u.Scheme != "http" {
-		return fmt.Errorf("scheme expected to be http")
+
+		parsedPort, err := strconv.ParseInt(port, 10, 64)
+		if err != nil {
+			return fmt.Errorf("invalid port in backend: %s", port)
+		}
+
+		if parsedPort > 65535 || parsedPort < 0 {
+			return fmt.Errorf("port '%s' must be in range [0, 65535]", port)
+		}
+
+		hostName = host
+	} else {
+		u, err := url.Parse(string(b))
+		if err != nil {
+			return fmt.Errorf("failed to parse backend with error: %w", err)
+		}
+
+		if u.Scheme != "http" {
+			return fmt.Errorf("scheme expected to be http")
+		}
+		hostName = u.Hostname()
 	}
 
-	ip := net.ParseIP(u.Hostname())
+	ip := net.ParseIP(hostName)
 	if len(ip) == 0 || ip.IsLoopback() {
-		return fmt.Errorf("invalid ip address in backend: %s", u.Hostname())
+		return fmt.Errorf("invalid ip address in backend: %s", hostName)
 	}
 	return nil
 }
---
+++ pkg/gridtypes/zos/gw_fqdn_test.go
@@ -0,0 +1,69 @@
+package zos
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestValidBackend(t *testing.T) {
+	require := require.New(t)
+
+	t.Run("test ip:port cases", func(t *testing.T) {
+		backend := Backend("1.1.1.1:10")
+		err := backend.Valid(true)
+		require.NoError(err)
+
+		backend = Backend("1.1.1.1:10")
+		err = backend.Valid(false)
+		require.Error(err)
+
+		backend = Backend("1.1.1.1:port")
+		err = backend.Valid(true)
+		require.Error(err)
+
+		backend = Backend("ip:10")
+		err = backend.Valid(true)
+		require.Error(err)
+
+		backend = Backend("1.1.1.1:1000000")
+		err = backend.Valid(true)
+		require.Error(err)
+
+		backend = Backend("1.1.1.1:-1")
+		err = backend.Valid(true)
+		require.Error(err)
+	})
+
+	t.Run("test http://ip:port cases", func(t *testing.T) {
+		backend := Backend("http://1.1.1.1:10")
+		err := backend.Valid(false)
+		require.NoError(err)
+
+		backend = Backend("http://1.1.1.1:10")
+		err = backend.Valid(true)
+		require.Error(err)
+
+		backend = Backend("http://1.1.1.1:port")
+		err = backend.Valid(false)
+		require.Error(err)
+
+		backend = Backend("http://ip:10")
+		err = backend.Valid(false)
+		require.Error(err)
+	})
+
+	t.Run("test http://ip cases", func(t *testing.T) {
+		backend := Backend("http://1.1.1.1")
+		err := backend.Valid(false)
+		require.NoError(err)
+
+		backend = Backend("http://1.1.1.1")
+		err = backend.Valid(true)
+		require.Error(err)
+
+		backend = Backend("http://ip")
+		err = backend.Valid(false)
+		require.Error(err)
+	})
+}
