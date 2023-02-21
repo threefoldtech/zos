35722b39535586241591626252996bec873d3588
Muhamad Azamy <muhamad@incubaid.com>
2023-02-21 11:41:58 +0100

    bump rmb version

--- bins/packages/rmb/rmb.sh
+++ bins/packages/rmb/rmb.sh
@@ -1,5 +1,5 @@
-RMB_VERSION="1.0.1-rc5"
-RMB_CHECKSUM="c52f6a42628f4039617d13e9962291e5"
+RMB_VERSION="1.0.1-rc6"
+RMB_CHECKSUM="117532f5da654d3a1cbe42e2edf9702e"
 RMB_LINK="https://github.com/threefoldtech/rmb-rs/releases/download/v${RMB_VERSION}/rmb-peer"
 
 download_rmb() {
