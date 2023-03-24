55f4e7b7450fa36df0181a4c848c5693b7124ba0
Muhamad Awad <muhamad@incubaid.com>
2023-03-24 08:53:48 +0100

    use the cloud-container from autobuilder (#1930)

--- pkg/primitives/vm/vm.go
+++ pkg/primitives/vm/vm.go
@@ -21,8 +21,7 @@ import (
 )
 
 const (
-	// this probably need a way to update. for now just hard code it
-	cloudContainerFlist = "https://hub.grid.tf/azmy.3bot/cloud-container-20221122.flist"
+	cloudContainerFlist = "https://hub.grid.tf/tf-autobuilder/cloud-container-8730b6f.flist"
 	cloudContainerName  = "cloud-container"
 )
 
