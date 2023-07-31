662f928bb8760f4ab823588ba9c12f2e0de8691d
mariobassem <mariobassem12@gmail.com>
2023-07-31 09:31:02 +0300

    add more geoip service urls
    Signed-off-by: mariobassem <mariobassem12@gmail.com>

--- pkg/geoip/geoip.go
+++ pkg/geoip/geoip.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"errors"
 	"net/http"
+
+	"github.com/rs/zerolog/log"
 )
 
 // Location holds the result of a geoip request
@@ -18,6 +20,8 @@ type Location struct {
 
 // Fetch retrieves the location of the system calling this function
 func Fetch() (Location, error) {
+	geoipURLs := []string{"https://geoip.grid.tf/", "https://02.geoip.grid.tf/", "https://03.geoip.grid.tf/"}
+
 	l := Location{
 		Longitute: 0.0,
 		Latitude:  0.0,
@@ -26,19 +30,26 @@ func Fetch() (Location, error) {
 		City:      "Unknown",
 	}
 
-	resp, err := http.Get("https://geoip.grid.tf")
-	if err != nil {
-		return l, err
-	}
-	defer resp.Body.Close()
+	for i := 0; i < len(geoipURLs); i++ {
+		resp, err := http.Get(geoipURLs[i])
+		if err != nil {
+			log.Err(err).Msgf("failed to make http call to geoip service %s. retrying...", geoipURLs[i])
+			continue
+		}
+		defer resp.Body.Close()
 
-	if resp.StatusCode != 200 {
-		return l, errors.New("error fetch location")
+		if resp.StatusCode != 200 {
+			log.Err(err).Msgf("geoip service %s responded with status %d. retrying...", geoipURLs[i], resp.StatusCode)
+			continue
+		}
+
+		if err := json.NewDecoder(resp.Body).Decode(&l); err != nil {
+			log.Err(err).Msgf("failed to decode location data from %s", geoipURLs[i])
+			continue
+		}
+
+		return l, nil
 	}
 
-	if err := json.NewDecoder(resp.Body).Decode(&l); err != nil {
-		return l, err
-	}
-
-	return l, err
+	return l, errors.New("failed to fetch location information")
 }
