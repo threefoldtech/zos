5e689cd6103311276644f68c76836fe0f5795a84
rawdaGastan <rody.rawda.rr@gmail.com>
2023-10-10 17:48:24 +0300

    add rfs bin

---
+++ bins/packages/rfs/rfs.sh
@@ -0,0 +1,30 @@
+RFS_VERSION="2.0.0"
+RFS_CHECKSUM="d6e1c3036d8b0d7d6df4d67fe17a0502"
+RFS_LINK="https://github.com/threefoldtech/rfs/releases/download/v${RFS_VERSION}/rfs"
+
+download_rfs() {
+    download_file ${RFS_LINK} ${RFS_CHECKSUM} rfs-${RFS_VERSION}
+}
+
+prepare_rfs() {
+    echo "[+] prepare 0-fs"
+    github_name "0-fs-${RFS_VERSION}"
+}
+
+install_rfs() {
+    echo "[+] install 0-fs"
+
+    mkdir -p "${ROOTDIR}/sbin"
+    cp -av rfs-${RFS_VERSION} "${ROOTDIR}/sbin/rfs"
+    chmod +x "${ROOTDIR}/sbin/rfs"
+}
+
+build_rfs() {
+    pushd "${DISTDIR}"
+
+    download_rfs
+    prepare_rfs
+    install_rfs
+
+    popd
+}
