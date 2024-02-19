70479976c8f107dd72683344cc8ddb1894f02fc6
Eslam-Nawara <eslamnawara44@gmail.com>
2024-02-19 16:54:39 +0200

    use mocking in boot.go

--- pkg/app/boot.go
+++ pkg/app/boot.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"io"
+	"io/fs"
 	"os"
 	"path/filepath"
 
@@ -8,17 +10,55 @@ import (
 )
 
 // bootedPath is the path where to store the booted flag
-var bootedPath = "/var/run/modules"
+const bootedPath = "/var/run/modules"
+
+// bootManager contains the path of the booted files and fileSystem
+type bootManager struct {
+	bootedPath string
+	fs         fileSystem
+}
+
+type fileSystem interface {
+	Create(string) (io.ReadCloser, error)
+	MkdirAll(string, uint32) error
+	Stat(string) (any, error)
+}
+
+type defaultBootManager struct{}
+
+func (fs defaultBootManager) Create(path string) (io.ReadCloser, error) {
+	return os.Create(path)
+}
+
+func (dfs defaultBootManager) MkdirAll(path string, perm uint32) error {
+	return os.MkdirAll(path, fs.FileMode(perm))
+}
+
+func (dfs defaultBootManager) Stat(path string) (any, error) {
+	return os.Stat(path)
+}
+
+func defaultMarkManager() *bootManager {
+	return &bootManager{
+		fs:         defaultBootManager{},
+		bootedPath: bootedPath,
+	}
+}
 
 // MarkBooted creates a file in a memory
 // this file then can be used to check if "something" has been restared
 // if its the first time it starts
 func MarkBooted(name string) error {
-	if err := os.MkdirAll(bootedPath, 0770); err != nil {
+	manager := defaultMarkManager()
+	return manager.markBooted(name)
+}
+
+func (m *bootManager) markBooted(name string) error {
+	if err := m.fs.MkdirAll(bootedPath, 0770); err != nil {
 		return err
 	}
 	path := filepath.Join(bootedPath, name)
-	marker, err := os.Create(path)
+	marker, err := m.fs.Create(path)
 	if err != nil {
 		return errors.Wrapf(err, "failed to mark service as booted: %s", name)
 	}
@@ -28,7 +68,12 @@ func MarkBooted(name string) error {
 
 // IsFirstBoot checks if the a file has been created by MarkBooted function
 func IsFirstBoot(name string) bool {
+	manager := defaultMarkManager()
+	return manager.isFirstBoot(name)
+}
+
+func (m *bootManager) isFirstBoot(name string) bool {
 	path := filepath.Join(bootedPath, name)
-	_, err := os.Stat(path)
+	_, err := m.fs.Stat(path)
 	return err != nil
 }
