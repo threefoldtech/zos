9a4f6db2b5c8859250dc6c8ec699275ed5d68f01
Eslam-Nawara <eslamnawara44@gmail.com>
2024-02-26 11:39:46 +0200

    remove logger tests

--- pkg/container/logger/logger.go
+++ pkg/container/logger/logger.go
@@ -2,7 +2,6 @@ package logger
 
 import (
 	"encoding/json"
-	"io/fs"
 	"os"
 )
 
@@ -30,68 +29,28 @@ type LogsRedis struct {
 	Stderr string `json:"stderr"`
 }
 
-type marshaler interface {
-	Marshal(any) ([]byte, error)
-	Unmarshal([]byte, any) error
-}
-
-var (
-	defMarshaler    = &defaultMarshaler{}
-	defReaderWriter = &defaultReaderWriter{}
-)
-
-type defaultMarshaler struct{}
-
-func (m *defaultMarshaler) Marshal(val any) ([]byte, error) {
-	return json.Marshal(val)
-}
-
-func (m *defaultMarshaler) Unmarshal(data []byte, str any) error {
-	return json.Unmarshal(data, str)
-}
-
-type readerWriter interface {
-	WriteFile(string, []byte, fs.FileMode) error
-	ReadFile(string) ([]byte, error)
-}
-
-type defaultReaderWriter struct{}
-
-func (rw *defaultReaderWriter) WriteFile(path string, data []byte, perm fs.FileMode) error {
-	return os.WriteFile(path, data, perm)
-}
-
-func (rw *defaultReaderWriter) ReadFile(path string) ([]byte, error) {
-	return os.ReadFile(path)
-}
-
 // Serialize dumps logs array into a json file
 func Serialize(path string, logs []Logs) error {
-	return serialize(path, logs, defMarshaler, defReaderWriter)
-}
-
-func serialize(path string, logs []Logs, m marshaler, rw readerWriter) error {
-	data, err := m.Marshal(logs)
+	data, err := json.Marshal(logs)
 	if err != nil {
 		return err
 	}
 
-	return rw.WriteFile(path, data, 0644)
+	return os.WriteFile(path, data, 0644)
 }
 
 // Deserialize reads json from disks and returns []Logs
 func Deserialize(path string) ([]Logs, error) {
-	return deserialize(path, defMarshaler, defReaderWriter)
-}
-
-func deserialize(path string, m marshaler, rw readerWriter) ([]Logs, error) {
 	logs := []Logs{}
 
-	data, err := rw.ReadFile(path)
+	data, err := os.ReadFile(path)
 	if err != nil {
 		return logs, err
 	}
 
-	err = m.Unmarshal(data, &logs)
-	return logs, err
+	if err := json.Unmarshal(data, &logs); err != nil {
+		return logs, err
+	}
+
+	return logs, nil
 }
