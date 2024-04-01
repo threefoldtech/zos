41284e61a6ad969d53848e06c7b34f7a2586fbc1
rawdaGastan <rody.rawda.rr@gmail.com>
2024-04-01 23:46:52 +0200

    add new kernel writer

--- pkg/primitives/vm/decompress_kernel.go
+++ pkg/primitives/vm/decompress_kernel.go
@@ -1,8 +1,23 @@
 package vm
 
-import "os/exec"
+import (
+	"io"
+	"os"
+	"os/exec"
+)
 
 func isValidELFKernel(KernelImagePath string) error {
 	_, err := exec.Command("readelf", "-h", KernelImagePath).CombinedOutput()
+	return err
+}
+
+func writer(reader io.Reader, targetPath string) error {
+	writer, err := os.Create(targetPath)
+	if err != nil {
+		return err
+	}
+	defer writer.Close()
+
+	_, err = io.Copy(writer, reader)
 	return err
 }
