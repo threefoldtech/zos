85e8aaab88805ccb2d9ebeff022d709d9180a4cc
Omar Abdulaziz <omarabdul3ziz@gmail.com>
2025-03-10 13:08:47 +0200

    pass zbus client to NewSystemMonitor
    method modified to accept zbus client due to feature in zoslight but it
    was not passed in zos3 cmds

--- cmds/modules/noded/main.go
+++ cmds/modules/noded/main.go
@@ -59,7 +59,6 @@ var Module cli.Command = cli.Command{
 }
 
 func registerationServer(ctx context.Context, msgBrokerCon string, env environment.Environment, info registrar.RegistrationInfo) error {
-
 	redis, err := zbus.NewRedisClient(msgBrokerCon)
 	if err != nil {
 		return errors.Wrap(err, "fail to connect to message broker server")
@@ -221,7 +220,7 @@ func action(cli *cli.Context) error {
 	}
 	go events.Start(ctx)
 
-	system, err := monitord.NewSystemMonitor(node, 2*time.Second)
+	system, err := monitord.NewSystemMonitor(node, 2*time.Second, redis)
 	if err != nil {
 		log.Fatal().Err(err).Msg("failed to initialize system monitor")
 	}
