27bf5e43ef167268913eaad6a852a648d5b8493f
Ashraf Fouda <ashraf.m.fouda@gmail.com>
2025-12-07 16:53:32 +0200

    update rfs 1.1.4
    Signed-off-by: Ashraf Fouda <ashraf.m.fouda@gmail.com>

--- bins/packages/rfs/rfs.sh
+++ bins/packages/rfs/rfs.sh
@@ -1,6 +1,6 @@
-RFS_VERSION_V1="1.1.1"
+RFS_VERSION_V1="1.1.4"
 RFS_VERSION_V2="2.0.6"
-RFS_CHECKSUM_V1="974b8dc45ae9c1b00238a79b0f4fc9de"
+RFS_CHECKSUM_V1="e13b70eae20f1bdf5c205c6252192ef2"
 RFS_CHECKSUM_V2="9e37743dfe94b4577ccf90c573c07638"
 RFS_LINK_V1="https://github.com/threefoldtech/rfs/releases/download/v${RFS_VERSION_V1}/rfs"
 RFS_LINK_V2="https://github.com/threefoldtech/rfs/releases/download/v${RFS_VERSION_V2}/rfs"
